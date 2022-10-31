680dcf43b68410f9f3179ecbd5e5d8b6bca8d02c
dabreegster <dabreegster@users.noreply.github.com>
2022-10-31 14:14:55 +0000

    deploy: e3d6edc17e51271fc5da91f5dcc49e92ba56e4ce

--- forms.js
+++ forms.js
@@ -1,10 +1,6 @@
-function checkbox(props, key, label) {
-  const checked = props[key] ? "checked" : "";
-  return `<input type="checkbox" id="${key}" ${checked}>
-		      <label for="${key}">${label}</label><br>`;
-}
+"use strict";
 
-function dropdown(props, key, label, values) {
+export function dropdown(props, key, label, values) {
   var html = `<div class="form-row">`;
   html += `<label for="${key}">${label}</label>`;
   html += `<select id="${key}">`;
@@ -16,4 +12,28 @@ function dropdown(props, key, label, values) {
   html += `</select>`;
   html += `</div>`;
   return html;
+}
+
+export function checkbox(props, key, label) {
+  const checked = props[key] ? "checked" : "";
+  return `<input type="checkbox" id="${key}" ${checked}>
+		      <label for="${key}">${label}</label><br>`;
+}
+
+export function checkboxes(props, prefix, labels) {
+  var result = ``;
+  var idx = 0;
+  for (const label of labels) {
+    const key = prefix + String.fromCharCode("a".charCodeAt(0) + idx);
+    result += checkbox(props, key, label);
+    idx += 1;
+  }
+  return result;
+}
+
+export function checkbox_ids(prefix, num) {
+  return Array.from(
+    { length: num },
+    (_, i) => prefix + String.fromCharCode("a".charCodeAt(0) + i)
+  );
 }
