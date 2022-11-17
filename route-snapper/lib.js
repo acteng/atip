92b545b6c08d0482f1f3b8e87269c1287a1b75ce
Dustin Carlino <dabreegster@gmail.com>
2022-11-17 11:11:31 +0000

    Add a progress bar for the route snapper download. #23
    https://javascript.info/fetch-progress used as reference

--- route-snapper/lib.js
+++ route-snapper/lib.js
@@ -149,3 +149,35 @@ export class RouteSnapper {
       .setData(JSON.parse(this.inner.renderGeojson()));
   }
 }
+
+export async function fetchWithProgress(url, progressId) {
+  const response = await fetch(url);
+  const reader = response.body.getReader();
+
+  const contentLength = response.headers.get("Content-Length");
+  const progressBar = document.getElementById(progressId);
+
+  let receivedLength = 0;
+  let chunks = [];
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+
+    chunks.push(value);
+    receivedLength += value.length;
+
+    const percent = (100.0 * receivedLength) / contentLength;
+    progressBar.style = `background: red; width: ${percent}%`;
+  }
+
+  let allChunks = new Uint8Array(receivedLength);
+  let position = 0;
+  for (let chunk of chunks) {
+    allChunks.set(chunk, position);
+    position += chunk.length;
+  }
+
+  return allChunks;
+}
