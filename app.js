e1f7b02f11fd2eff5ceedee342dd1def90622a84
dabreegster <dabreegster@users.noreply.github.com>
2022-11-22 15:25:37 +0000

    deploy: 5c90b70f4604c94881f85363da5c2889c303aceb

--- app.js
+++ app.js
@@ -154,6 +154,15 @@ export class App {
         }
       });
 
+      // When the user modifies an existing mapbox-gl-draw object, keep the "editing" layer in sync.
+      // This doesn't happen in the middle of the drag; there's no event for that
+      this.map.on("draw.update", (e) => {
+        this.map.getSource("editing").setData({
+          type: "FeatureCollection",
+          features: [e.features[0]],
+        });
+      });
+
       this.routeSnapper = await setupRouteSnapper(this);
     });
 
