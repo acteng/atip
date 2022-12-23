8c1840031cec7705dec32d0926d1a5d5d27be105
Dustin Carlino <dabreegster@gmail.com>
2022-12-23 19:20:12 +0000

    Tell vite about the other HTML page :)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "scheme.html"),
+      },
+    },
+  },
+  base: "/atip/",
   plugins: [
     wasmPack([], ["route-snapper"]),
     // Remove when https://caniuse.com/?search=top%20level%20await is more green
@@ -11,5 +21,4 @@ export default defineConfig({
       promiseImportName: (i) => `__tla_${i}`,
     }),
   ],
-  base: "/atip/",
 });
