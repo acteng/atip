a876bc14a93f48494afa260139272de5af66cfc0
Dustin Carlino <dabreegster@gmail.com>
2022-12-24 17:39:28 +0000

    Get that sidebar going... kind of. the CSS is so broken

--- SchemePage.svelte
+++ SchemePage.svelte
@@ -2,6 +2,7 @@
   import About from "./components/About.svelte";
   import Instructions from "./components/Instructions.svelte";
   import Map from "./components/Map.svelte";
+  import Layout from "./components/Layout.svelte";
 
   let hideAbout = true;
   let hideInstructions = true;
@@ -16,13 +17,18 @@
   }
 </script>
 
-<p>Heya</p>
-<nav>
-  <button type="button" on:click={toggleAbout}>About</button>
-  <button type="button" on:click={toggleInstructions}>Instructions</button>
-</nav>
+<Layout>
+  <div slot="nav">
+    <button type="button" on:click={toggleAbout}>About</button>
+    <button type="button" on:click={toggleInstructions}>Instructions</button>
+  </div>
+  <div slot="sidebar">
+    <p>Heya</p>
+  </div>
+  <div slot="main">
+    <Map />
+  </div>
+</Layout>
+
 <About bind:hidden={hideAbout} />
 <Instructions bind:hidden={hideInstructions} />
-<Map />
-
-<style></style>
