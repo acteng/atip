effd2813d57d6a09d876b65698d2906665a27dfd
Dustin Carlino <dabreegster@gmail.com>
2022-12-26 12:32:42 +0000

    Attempt the form. Quite broken.

---
+++ components/Form.svelte
@@ -0,0 +1,57 @@
+<script>
+  export let feature;
+
+  function capitalize(x) {
+    return x.charAt(0).toUpperCase() + x.slice(1);
+  }
+</script>
+
+<br />
+
+<label>
+  Name<br />
+  <input type="text" size="30" value={feature.properties.name} />
+</label>
+
+<br />
+
+<div class="radio">
+  {#each ["area", "route", "crossing", "other"] as value}
+    <label>
+      <input
+        type="radio"
+        bind:group={feature.properties.intervention_type}
+        name="type"
+        {value}
+      />
+      {capitalize(value)}
+    </label>
+  {/each}
+</div>
+
+<br />
+
+<label>
+  Description<br />
+  <textarea rows="3" cols="40">{feature.properties.Description}</textarea>
+</label>
+
+<br />
+
+<div>
+  <button type="button">Delete</button>
+  <button type="button" style="float: right;">Save</button>
+</div>
+
+<br />
+
+<style>
+  .radio {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  textarea {
+    resize: none;
+  }
+</style>
