68ae0c30a0076f76b210dfeef02925f717c3cf07
Dustin Carlino <dabreegster@gmail.com>
2023-01-04 15:55:43 +0000

    Update about page with feedback links

--- components/About.svelte
+++ components/About.svelte
@@ -1,5 +1,10 @@
 <script>
-  import { Modal, Link } from "carbon-components-svelte";
+  import {
+    Modal,
+    Link,
+    UnorderedList,
+    ListItem,
+  } from "carbon-components-svelte";
 
   export let open;
 </script>
@@ -12,23 +17,31 @@
     >
     developed by:
   </p>
-  <ul>
-    <li>
-      <Link href="https://dcarlino.org" target="_blank">Dustin Carlino</Link>:
-      lead developer
-    </li>
-    <li>
+
+  <br />
+
+  <UnorderedList>
+    <ListItem>
+      <Link
+        href="https://www.turing.ac.uk/people/researchers/dustin-carlino"
+        target="_blank">Dustin Carlino</Link
+      >: lead developer, from The Alan Turing Institute
+    </ListItem>
+    <ListItem>
       <Link
         href="https://www.linkedin.com/in/madison-wang-841977bb/"
         target="_blank">Madison Wang</Link
       >: UX design
-    </li>
-    <li>
+    </ListItem>
+    <ListItem>
       <Link href="https://github.com/BudgieInWA" target="_blank"
         >Ben Ritter</Link
       >: CSS consulting
-    </li>
-  </ul>
+    </ListItem>
+  </UnorderedList>
+
+  <br />
+
   <p>
     ATIP builds on
     <Link href="https://www.openstreetmap.org/about" target="_blank"
@@ -38,5 +51,16 @@
     <Link href="https://maplibre.org/" target="_blank">MapLibre</Link>,
     <Link href="https://georust.org/" target="_blank">GeoRust</Link>, and other
     open source projects.
+  </p>
+
+  <br />
+
+  <p>
+    We want your feedback about ATIP! Please <Link
+      href="https://github.com/acteng/atip/issues/new"
+      target="_blank">start an issue on Github</Link
+    > or email <Link href="mailto: dcarlino@turing.ac.uk"
+      >dcarlino@turing.ac.uk</Link
+    >.
   </p>
 </Modal>
