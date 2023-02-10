06ff1737b9becf5ae2c98d60c2867649f1579f7d
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-10 09:00:05 +0000

    convert to arrow function

--- ChooseArea.svelte
+++ ChooseArea.svelte
@@ -89,9 +89,9 @@
   loadAuthorities();
 
   function start() {
-    let auth_names = Array.from(dataList.childNodes.values()).map(function (x) {
-      return x.value;
-    });
+    let auth_names = Array.from(dataList.childNodes.values()).map(
+      (x) => x.value
+    );
     console.log();
     if (auth_names.includes(inputList.value)) {
       window.location.href = `scheme.html?authority=${inputList.value}`;
