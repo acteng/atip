5335ae53e9ba2867a7d978c53ece48f0c3f19718
Dustin Carlino <dabreegster@gmail.com>
2023-03-18 18:37:52 +0000

    Simplify end-to-end tests.
    - Remove vitest; playwright as a test runner seems fine
    - Start the local dev server using common config
    - Consolidate the two tests so far

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ You'll need
 [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm). Run
 `npm install` once. Use `npm run dev` to work locally. To auto-format code,
 `npm run fmt`.
+
+## Tests
+
+Run all tests: `npm run test`
+
+To develop a test interactively, `npm run test -- --debug`. You can step
+through a test line-by-line with a browser open. Press "record" and then
+interact with the page normally, generating the equivalent test code.
---
+++ tests/homepage.spec.js
@@ -0,0 +1,22 @@
+import { preview } from "vite";
+import { test, expect } from "@playwright/test";
+
+test("front page has welcome text", async ({ page }) => {
+  await page.goto("/");
+  await expect(
+    page.getByRole("heading", { name: "Welcome to ATIP v1" })
+  ).toBeVisible();
+});
+
+test("choosing a local authority and clicking start changes the url", async ({
+  page,
+}) => {
+  await page.goto("/");
+  await page
+    .getByTestId("transport-authority")
+    .fill("West Yorkshire Combined Authority");
+  await page.getByRole("button", { name: "Start" }).click();
+  await expect(page).toHaveURL(
+    /.*scheme.html\?authority=West%20Yorkshire%20Combined%20Authority/
+  );
+});
