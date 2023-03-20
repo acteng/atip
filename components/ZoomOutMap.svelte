0fe8e0aa4918f4914e34d5665996e767336125e4
Dustin Carlino <dabreegster@gmail.com>
2023-03-20 15:04:09 +0000

    Add a button to recenter the view on the entire boundary

---
+++ components/ZoomOutMap.svelte
@@ -0,0 +1,19 @@
+<script>
+  import geojsonExtent from "@mapbox/geojson-extent";
+  import { map } from "../stores.js";
+  import icon from "../assets/zoom_out_map.svg";
+
+  export let boundaryGeojson;
+
+  function recenter() {
+    $map.fitBounds(geojsonExtent(boundaryGeojson), {
+      padding: 20,
+      animate: true,
+      duration: 500,
+    });
+  }
+</script>
+
+<button type="button" title="Zoom to show entire boundary" on:click={recenter}>
+  <img src={icon} alt="Zoom to show entire boundary" />
+</button>
