0d2d689e3166666ebae0021cb449ae3c4cd5be29
Dustin Carlino <dabreegster@gmail.com>
2023-03-23 15:21:00 +0000

    Add a control to switch baselayers to satellite. #51

---
+++ components/BaselayerSwitcher.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let style;
+
+  function changeStyle() {
+    let params = new URLSearchParams(window.location.search);
+    params.set("style", style);
+    let href = `${window.location.pathname}?${params.toString()}${
+      window.location.hash
+    }`;
+    window.location.href = href;
+  }
+</script>
+
+<div>
+  Basemap:
+  <select bind:value={style} on:change={changeStyle}>
+    <option value="streets">Streets</option>
+    <option value="hybrid">Satellite</option>
+  </select>
+</div>
+
+<style>
+  div {
+    position: absolute;
+    left: 10px;
+    bottom: 50px;
+    background-color: white;
+    padding: 16px;
+  }
+</style>
