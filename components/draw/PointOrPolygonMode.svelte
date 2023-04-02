cea1a72a7de88d8dcd7d6fe70a2a46abeb0d80b5
Dustin Carlino <dabreegster@gmail.com>
2023-04-02 12:04:08 +0100

    Do the same for polygons

--- components/draw/PointOrPolygonMode.svelte
+++ components/draw/PointOrPolygonMode.svelte
@@ -4,11 +4,13 @@
   import { map, gjScheme, newFeatureId } from "../../stores.js";
 
   export let mode;
+  // "point" or "polygon"
+  export let type;
   let drawControls;
 
   // When mode changes, make the controls appear or disappear
   $: {
-    if (mode == "point") {
+    if (mode == type) {
       start();
     } else {
       stop();
@@ -17,15 +19,35 @@
 
   onDestroy(stop);
 
+  $map.on("draw.create", (e) => {
+    // Be careful; this callback fires for the other PointOrPolygonMode instance!
+    if (mode == type) {
+      // Assume there's exactly 1 feature
+      const feature = e.features[0];
+
+      gjScheme.update((gj) => {
+        feature.properties.intervention_type =
+          type == "point" ? "other" : "area";
+        feature.id = newFeatureId(gj);
+        gj.features.push(feature);
+        return gj;
+      });
+
+      // TODO setCurrentlyEditing to this new thing
+      mode = "select";
+    }
+  });
+
   function start() {
     drawControls = new MapboxDraw({
       displayControlsDefault: false,
       controls: {
-        point: true,
+        point: type == "point",
+        polygon: type == "polygon",
       },
     });
     $map.addControl(drawControls);
-    drawControls.changeMode("draw_point");
+    drawControls.changeMode(`draw_${type}`);
     // mapbox-gl-draw inserts a button to control modes; destroy it
     let elements = document.getElementsByClassName(
       "mapboxgl-ctrl-group mapboxgl-ctrl"
@@ -34,21 +56,6 @@
     while (elements.length > 0) {
       elements[0].remove();
     }
-
-    $map.on("draw.create", (e) => {
-      // Assume there's exactly 1 feature
-      const feature = e.features[0];
-
-      gjScheme.update((gj) => {
-        feature.properties.intervention_type = "other";
-        feature.id = newFeatureId(gj);
-        gj.features.push(feature);
-        return gj;
-      });
-
-      // TODO setCurrentlyEditing to this new thing
-      mode = "select";
-    });
   }
 
   function stop() {
@@ -59,6 +66,12 @@
   }
 </script>
 
-{#if mode == "point"}
+{#if mode == "point" && type == mode}
   <p>Click the map to draw a new point</p>
+{:else if mode == "polygon" && type == mode}
+  <p>
+    Click the map to add a vertex. Click the last vertex or press <strong
+      >Enter</strong
+    > to finish.
+  </p>
 {/if}
