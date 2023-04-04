0c732ba900c22a6c853ab52a1c6abe42a85f803b
Dustin Carlino <dabreegster@gmail.com>
2023-04-04 14:36:15 +0100

    Move EditingLayer's functionality into AttributeMode

--- components/draw/AttributeMode.svelte
+++ components/draw/AttributeMode.svelte
@@ -1,9 +1,12 @@
 <script>
+  import geojsonExtent from "@mapbox/geojson-extent";
   import {
     map,
+    gjScheme,
     currentHover,
     currentlyEditing,
     setCurrentlyEditing,
+    openFromSidebar,
   } from "../../stores.js";
 
   export let mode;
@@ -53,6 +56,25 @@
         setCurrentlyEditing(results[0].id);
       } else {
         setCurrentlyEditing(null);
+      }
+    }
+  });
+
+  // When the user starts editing something from the sidebar, warp to what's
+  // being edited. (Don't do this when clicking the object on the map.)
+  openFromSidebar.subscribe((id) => {
+    if (mode == "edit-attribute" && id) {
+      let feature = $gjScheme.features.find((f) => f.id == id);
+
+      // Extent of points is defined in a weird way, special-case it
+      if (feature.geometry.type == "Point") {
+        $map.flyTo({ center: feature.geometry.coordinates });
+      } else {
+        $map.fitBounds(geojsonExtent(feature), {
+          padding: 200,
+          animate: true,
+          duration: 500,
+        });
       }
     }
   });
