ca36af0ecb95999461415588400802032bdf516d
Dustin Carlino <dabreegster@gmail.com>
2023-04-06 16:35:22 +0100

    Move point drawing logic into a regular JS class, and use it from new or edit modes, similar to the route snapper. #58

--- components/draw/PointMode.svelte
+++ components/draw/PointMode.svelte
@@ -3,81 +3,35 @@
     map,
     gjScheme,
     newFeatureId,
-    emptyGeojson,
     setCurrentlyEditing,
   } from "../../stores.js";
-  import {
-    overwriteSource,
-    overwriteLayer,
-    drawCircle,
-  } from "../../maplibre_helpers.js";
-  import { colors, circleRadius } from "../../colors.js";
 
   const thisMode = "point";
 
   export let mode;
   export let changeMode;
+  export let pointTool;
 
-  // An optional Feature<Point>
-  let cursor = null;
-
-  export function start() {}
+  export function start() {
+    pointTool.start();
+  }
   export function stop() {
-    cursor = null;
+    pointTool.stop();
   }
 
-  $map.on("mousemove", (e) => {
+  pointTool.addEventListener((feature) => {
     if (mode == thisMode) {
-      cursor = pointFeature(e.lngLat.toArray());
-    }
-  });
-  $map.on("click", (e) => {
-    // TODO is it possible cursor is null?
-    if (mode == thisMode && cursor) {
       gjScheme.update((gj) => {
-        cursor.id = newFeatureId(gj);
-        cursor.properties.intervention_type = "other";
-        gj.features.push(cursor);
+        feature.id = newFeatureId(gj);
+        feature.properties.intervention_type = "other";
+        gj.features.push(feature);
         return gj;
       });
 
-      // Remember before stop()
-      let id = cursor.id;
       changeMode("edit-attribute");
-      setCurrentlyEditing(id);
+      setCurrentlyEditing(feature.id);
     }
   });
-
-  // Render
-  const source = "edit-point-mode";
-  overwriteSource($map, source, {
-    type: "geojson",
-    data: emptyGeojson(),
-  });
-  overwriteLayer($map, {
-    id: "edit-point-mode",
-    source,
-    ...drawCircle(colors.hovering, circleRadius, 1.0),
-  });
-
-  $: {
-    let gj = emptyGeojson();
-    if (cursor) {
-      gj.features.push(cursor);
-    }
-    $map.getSource(source).setData(gj);
-  }
-
-  function pointFeature(pt) {
-    return {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        type: "Point",
-        coordinates: pt,
-      },
-    };
-  }
 </script>
 
 {#if mode == thisMode}
