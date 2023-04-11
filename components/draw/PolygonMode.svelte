52e404a022fd576a31c5eb0be714d367c2781954
Dustin Carlino <dabreegster@gmail.com>
2023-04-11 19:45:47 +0100

    Start a tool to draw polygons. It's minimally usable. #58
    (https://github.com/a-b-street/abstreet/blob/main/map_gui/src/tools/polygon.rs
    as reference!)

---
+++ components/draw/PolygonMode.svelte
@@ -0,0 +1,43 @@
+<script>
+  import {
+    map,
+    gjScheme,
+    newFeatureId,
+    setCurrentlyEditing,
+  } from "../../stores.js";
+
+  const thisMode = "polygon";
+
+  export let mode;
+  export let changeMode;
+  export let polygonTool;
+
+  export function start() {
+    polygonTool.startNew();
+  }
+  export function stop() {
+    polygonTool.stop();
+  }
+
+  polygonTool.addEventListener((feature) => {
+    if (mode == thisMode) {
+      gjScheme.update((gj) => {
+        feature.id = newFeatureId(gj);
+        feature.properties.intervention_type = "area";
+        gj.features.push(feature);
+        return gj;
+      });
+
+      changeMode("edit-attribute");
+      setCurrentlyEditing(feature.id);
+    }
+  });
+</script>
+
+{#if mode == thisMode}
+  <p>
+    Click the map to add a vertex. Click the last vertex or press <strong
+      >Enter</strong
+    > to finish. Drag a vertex or the polygon to move.
+  </p>
+{/if}
