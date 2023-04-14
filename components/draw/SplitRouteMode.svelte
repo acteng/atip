11638a14d6c16edc8e58eefcdbe811877bbce7d7
Dustin Carlino <dabreegster@gmail.com>
2023-04-14 11:18:04 +0100

    Decide snapped/free split waypoints more carefully.

--- components/draw/SplitRouteMode.svelte
+++ components/draw/SplitRouteMode.svelte
@@ -156,6 +156,8 @@
 
     let splitDist = distanceAlongLine(original, splitPt);
     let firstPiece = true;
+    // TODO Can we iterate over an array's contents and get the index at the same time?
+    let i = 0;
     for (let waypt of original.properties.waypoints) {
       let wayptDist = distanceAlongLine(
         original,
@@ -165,28 +167,45 @@
         if (wayptDist < splitDist) {
           piece1.properties.waypoints.push(waypt);
         } else {
-          // We found where the split occurs. Fix piece1
+          // We found where the split occurs. We'll insert a new waypoint
+          // representing the split at the end of piece1 and the beginning of
+          // piece2. Should that new waypoint be snapped or freehand? There are
+          // 4 cases for where the split (|) happens with regards to a
+          // (s)napped and (f)reehand point:
+          //
+          // 1) s | s
+          // 2) s | f
+          // 3) f | s
+          // 4) f | f
+          //
+          // Only in case 1 should the new waypoint introduced at (|) be
+          // snapped.
+          // TODO Problem: in case 1, what if we split in the middle of a road,
+          // far from an intersection?
+
+          // Note i > 0; splitDist can't be before the first waypoint (distance 0)
+          // TODO Edge case: somebody manages to exactly click a waypoint
+          let snapped =
+            waypt.snapped && original.properties.waypoints[i - 1].snapped;
+
           piece1.properties.waypoints.push({
             lon: splitPt.geometry.coordinates[0],
             lat: splitPt.geometry.coordinates[1],
-            // TODO Problem: even if we're sandwiched between two snapped
-            // waypoints, what if we split in the middle of a road, far from an
-            // intersection?
-            snapped: true,
+            snapped,
           });
 
-          // Fix piece2
           firstPiece = false;
           piece2.properties.waypoints.push({
             lon: splitPt.geometry.coordinates[0],
             lat: splitPt.geometry.coordinates[1],
-            snapped: true,
+            snapped,
           });
           piece2.properties.waypoints.push(waypt);
         }
       } else {
         piece2.properties.waypoints.push(waypt);
       }
+      i++;
     }
   }
 
