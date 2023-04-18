5cdecdee9fdeb7a6e5997639b3575e11f285b1ba
Dustin Carlino <dabreegster@gmail.com>
2023-04-18 09:18:45 +0100

    Use our own modal, not Carbon's

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let title: string;
+  export let open = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="background"
+  on:click={() => (open = false)}
+  class:open
+  class:closed={!open}
+>
+  <div
+    class="content centered-fullscreen"
+    on:click|stopPropagation={() => null}
+  >
+    <div style="display: flex">
+      <h1>{title}</h1>
+      <button on:click={() => (open = false)}>X</button>
+    </div>
+    <slot />
+  </div>
+</div>
+
+<style>
+  .background {
+    position: fixed;
+    z-index: 10;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: block;
+  }
+
+  button {
+    background-color: whitesmoke;
+  }
+
+  .content {
+    background: white;
+    padding: 30px;
+    border: 1px solid black;
+  }
+
+  .centered-fullscreen {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    overflow: scroll;
+  }
+
+  .open {
+    display: block;
+  }
+
+  .closed {
+    display: none;
+  }
+</style>
