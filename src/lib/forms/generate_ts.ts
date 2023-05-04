3619a5f4d0515c6675eef49a700626a60f1a52ea
Dustin Carlino <dabreegster@gmail.com>
2023-05-04 09:45:09 +0100

    Port generate_ts to TS. Mostly just a build configuration headache.

--- src/lib/forms/generate_ts.ts
+++ src/lib/forms/generate_ts.ts
@@ -1,19 +1,29 @@
 import { argv } from "node:process";
 import * as fs from "fs";
+// TODO Normally we leave off the file extension, but ts-node config is somehow wrong
+import {
+  type Field,
+  isStruct,
+  isEnum,
+  isOneLiner,
+  isTextbox,
+  isNumber,
+  isCheckbox,
+} from "./types.js";
 
 // This script transforms a schema expressed in JSON into TypeScript types
 
 let schema = JSON.parse(fs.readFileSync(argv[2], { encoding: "utf8" }));
 // Queue contains all of the types to generate
-let queue = [schema];
-let seen = new Set();
+let queue: Field[] = [schema];
+let seen: Set<string> = new Set();
 
 console.log(`// This file is auto-generated; do not manually edit\n`);
 while (queue.length > 0) {
-  generate(queue.pop());
+  generate(queue.pop()!);
 }
 
-function generate(field) {
+function generate(field: Field) {
   if (seen.has(field.name)) {
     // We could also generate more detailed type names based on the nesting,
     // but this seems confusing
@@ -21,38 +31,33 @@ function generate(field) {
   }
   seen.add(field.name);
 
-  // TODO Use isStruct and co, after this script is TS and not JS
-  if ("members" in field) {
-    // Struct
+  if (isStruct(field)) {
     console.log(`export interface ${field.name} {`);
     for (let member of field.members) {
-      if ("type" in member) {
-        if (member.type == "number") {
-          console.log(`  ${member.name}?: number;`);
-        } else if (member.type == "one-liner" || member.type == "textbox") {
-          console.log(`  ${member.name}?: string;`);
-        } else if (member.type == "checkbox") {
-          console.log(`  ${member.name}?: boolean;`);
-        } else {
-          throw new Error(`Unknown field type ${member.type}`);
-        }
-      } else {
+      if (isNumber(member)) {
+        console.log(`  ${member.name}?: number;`);
+      } else if (isOneLiner(member) || isTextbox(member)) {
+        console.log(`  ${member.name}?: string;`);
+      } else if (isCheckbox(member)) {
+        console.log(`  ${member.name}?: boolean;`);
+      } else if (isStruct(member) || isEnum(member)) {
         // TODO If it's an enum with all string cases, do we want another
         // explicitly named type or not?
         queue.push(member);
         console.log(`  ${member.name}?: ${member.name};`);
+      } else {
+        throw new Error(`Unknown field type ${member}`);
       }
     }
     console.log(`}\n`);
-  } else if ("oneOf" in field) {
-    // Enum
+  } else if (isEnum(field)) {
     let cases = [];
     for (let x of field.oneOf) {
-      if (x.name) {
+      if (typeof x == "string") {
+        cases.push(`"${x}"`);
+      } else {
         cases.push(x.name);
         queue.push(x);
-      } else {
-        cases.push(`"${x}"`);
       }
     }
     console.log(`export type ${field.name} = ${cases.join(" | ")};\n`);
