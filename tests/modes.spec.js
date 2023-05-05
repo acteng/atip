f8da085d75c3fc6b92a7391d21e0561f548f35c6
Dustin Carlino <dabreegster@gmail.com>
2023-05-05 14:24:18 +0100

    Add a test to reproduce a bug with clearing features while a form is open

---
+++ tests/modes.spec.js
@@ -0,0 +1,22 @@
+import { readFile } from "fs/promises";
+import { test, expect } from "@playwright/test";
+
+test("clearing all while a feature is open works", async ({ page }) => {
+  // TODO Fix the bug!
+  test.fail();
+
+  await page.goto("/scheme.html?authority=North Somerset");
+  await page.getByRole("button", { name: "New point" }).click();
+  await page.getByRole("region", { name: "Map" }).click({
+    position: {
+      x: 500,
+      y: 500,
+    },
+  });
+  page.on("dialog", (dialog) => dialog.accept());
+  await page.getByRole("button", { name: "Clear all" }).click();
+  // Make sure AttributeMode resets to nothing being selected
+  await expect(
+    page.getByText("Click an intervention to fill out its attributes")
+  ).toBeVisible();
+});
