a4dccf94f06a76243f42d3099484ebc0b47d7dc3
Dustin Carlino <dabreegster@gmail.com>
2023-05-18 15:10:52 +0100

    Disable map-wide layers when any form is open

--- src/lib/layers/ContextualLayers.svelte
+++ src/lib/layers/ContextualLayers.svelte
@@ -2,19 +2,27 @@
   import { type Remote } from "comlink";
   import { type RouteInfo } from "../../worker";
   import SpeedLimits from "./SpeedLimits.svelte";
+  import { formOpen } from "../../stores";
 
   export let routeInfo: Remote<RouteInfo>;
 
   let show: "none" | "speed limits" = "none";
+
+  // If any form is open, don't show a map-wide layer.
+  $: {
+    if ($formOpen) {
+      show = "none";
+    }
+  }
 </script>
 
 <label>
   Show layer:
-  <select bind:value={show}>
+  <select bind:value={show} disabled={$formOpen != null}>
     <option value="none">None</option>
     <option value="speed limits">Speed limits</option>
   </select>
 </label>
 {#if show == "speed limits"}
-  <SpeedLimits {routeInfo} />
+  <SpeedLimits {routeInfo} id={undefined} />
 {/if}
