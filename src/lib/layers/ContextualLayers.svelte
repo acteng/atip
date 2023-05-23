8678c52837b36af1da687ebf50c2536f6cf43ac6
Dustin Carlino <dabreegster@gmail.com>
2023-05-23 13:08:59 +0100

    Show all layers, and add an explanatory tooltip.
    Remove the code for rendering everything; there's no way it's going to make it into this first cut

--- src/lib/layers/ContextualLayers.svelte
+++ src/lib/layers/ContextualLayers.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import SpeedLimits from "./SpeedLimits.svelte";
-  import LaneDetails from "./LaneDetails.svelte";
   import { formOpen } from "../../stores";
 
-  let show: "none" | "speed limits" | "lane details" = "none";
+  let show: "none" | "speed limits" = "none";
 
   // If any form is open, don't show a map-wide layer.
   $: {
@@ -18,11 +17,8 @@
   <select bind:value={show} disabled={$formOpen != null}>
     <option value="none">None</option>
     <option value="speed limits">Speed limits</option>
-    <option value="lane details">Lane details</option>
   </select>
 </label>
 {#if show == "speed limits"}
   <SpeedLimits id={undefined} />
-{:else if show == "lane details"}
-  <LaneDetails />
 {/if}
--- src/lib/layers/LaneDetails.svelte
+++ src/lib/layers/LaneDetails.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+  import type { LineString } from "geojson";
+  import type { Feature } from "../../types";
   import { onMount } from "svelte";
   import { gjScheme, routeInfo } from "../../stores";
   import LanePolygons from "./lane_details/LanePolygons.svelte";
   import LaneMarkings from "./lane_details/LaneMarkings.svelte";
+  import IntersectionPolygons from "./lane_details/IntersectionPolygons.svelte";
+  import IntersectionMarkings from "./lane_details/IntersectionMarkings.svelte";
+  import HelpIcon from "../common/HelpIcon.svelte";
 
-  // Show along a route if specified, or show all otherwise
-  export let id: number | undefined;
+  // Show along a route
+  export let id: number;
 
   let gj1;
   let gj2;
@@ -14,21 +19,16 @@
 
   onMount(async () => {
     try {
-      let raw;
-      if (id) {
-        let linestring = $gjScheme.features.find(
-          (f) => f.id == id
-        ) as Feature<LineString>;
-        raw = await $routeInfo.renderLaneDetailsForRoute(
-          linestring.properties.waypoints
-        );
-      } else {
-        raw = await $routeInfo.renderAllLaneDetails();
-      }
+      let linestring = $gjScheme.features.find(
+        (f) => f.id == id
+      ) as Feature<LineString>;
+      let raw = await $routeInfo.renderLaneDetailsForRoute(
+        linestring.properties.waypoints
+      );
       gj1 = JSON.parse(raw[0]);
       gj2 = JSON.parse(raw[1]);
-      //gj3 = JSON.parse(raw[2]);
-      //gj4 = JSON.parse(raw[3]);
+      gj3 = JSON.parse(raw[2]);
+      gj4 = JSON.parse(raw[3]);
     } catch (e) {
       window.alert(`Couldn't render lane details: ${e}`);
     }
@@ -38,4 +38,10 @@
 {#if gj1}
   <LanePolygons gj={gj1} />
   <LaneMarkings gj={gj2} />
+  <IntersectionPolygons gj={gj3} />
+  <IntersectionMarkings gj={gj4} />
 {/if}
+
+<HelpIcon
+  contents="This visualizes lane data according to OpenStreetMap. There may be many errors with incorrect or missing lane data, lane width and intersection geometry, and markings. Use with caution."
+/>
