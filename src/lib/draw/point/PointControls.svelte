3d590435a5cc6789805d63fbeddb03d3fc14149b
Dustin Carlino <dabreegster@gmail.com>
2023-05-25 12:20:21 +0100

    Use a Help card for point and split route controls, for consistency

--- src/lib/draw/point/PointControls.svelte
+++ src/lib/draw/point/PointControls.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
   import { PointTool } from "./point_tool";
   import KeyHandler from "../KeyHandler.svelte";
+  import CollapsibleCard from "../../common/CollapsibleCard.svelte";
 
   export let pointTool: PointTool;
   export let editingExisting: boolean;
 </script>
 
-<ul>
-  {#if editingExisting}
-    <li>Click to move the point here</li>
-  {:else}
-    <li>Click to add a new point</li>
-  {/if}
-  <li>Press <b>Escape</b> to cancel</li>
-</ul>
+<CollapsibleCard label="Help">
+  <ul>
+    {#if editingExisting}
+      <li>Click to move the point here</li>
+    {:else}
+      <li>Click to add a new point</li>
+    {/if}
+    <li>Press <b>Escape</b> to cancel</li>
+  </ul>
+</CollapsibleCard>
 
 <button type="button" on:click={() => pointTool.cancel()}>Cancel</button>
 <KeyHandler tool={pointTool} />
