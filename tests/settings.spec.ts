88b6db80c443980da87ac4814732258b15d9b385
Dustin Carlino <dabreegster@gmail.com>
2023-06-01 13:29:00 +0100

    Test user settings are preserved

---
+++ tests/settings.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("changed settings are preserved across refresh", async ({ page }) => {
+  await page.goto("/scheme.html?authority=Adur");
+  await page.getByRole("button", { name: "New route" }).click();
+  await expect(page.getByLabel("Avoid doubling back")).not.toBeChecked();
+  await page.getByLabel("Avoid doubling back").check();
+
+  await page.reload();
+  await page.getByRole("button", { name: "New route" }).click();
+  await expect(page.getByLabel("Avoid doubling back")).toBeChecked();
+});
