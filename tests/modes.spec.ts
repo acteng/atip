4ee9399845b31f0ac397561f192439f8baee9082
Dustin Carlino <dabreegster@gmail.com>
2023-06-01 13:36:15 +0100

    Start a test for #207

--- tests/modes.spec.ts
+++ tests/modes.spec.ts
@@ -72,3 +72,20 @@ test("creating a new route opens a form", async ({ page }) => {
   await page.getByRole("button", { name: "1) Untitled route" }).isVisible();
   await page.getByLabel("Description:").click();
 });
+
+test("other tools work when route tool doesn't load", async ({ page }) => {
+  await page.route("https://atip.uk/route-snappers-dev/Adur.bin.gz", (route) =>
+    route.fulfill({
+      status: 404,
+    })
+  );
+  await page.goto("/scheme.html?authority=Adur");
+
+  await page.getByRole("button", { name: "New route" }).click();
+  await expect(page.getByText("Failed to load")).toBeVisible();
+
+  // Other tools should still work
+  await page.getByRole("button", { name: "New point" }).click();
+  await clickMap(page, 500, 500);
+  await page.getByRole("button", { name: "1) Untitled point" }).isVisible();
+});
