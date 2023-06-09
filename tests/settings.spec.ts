bfc8e4e642783bea8bf39ce8508f93756b507efd
Pete Y <peter.york@activetravelengland.gov.uk>
2023-06-09 15:44:46 +0100

    Attempt to make setting change test more resilient.

--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -5,10 +5,22 @@ test("changed settings are preserved across refresh", async ({ browser }) => {
   let page = await loadInitialPage(browser);
   await page.goto("/scheme.html?authority=Adur");
   await page.getByRole("button", { name: "New route" }).click();
-  await expect(page.getByLabel("Avoid doubling back")).not.toBeChecked();
-  await page.getByLabel("Avoid doubling back").check();
+  let expectedToBeChecked = false;
+  if (!(await page.getByLabel("Avoid doubling back").isChecked())) {
+    await page.getByLabel("Avoid doubling back").check();
+    await expect(page.getByLabel("Avoid doubling back")).toBeChecked();
+    expectedToBeChecked = true;
+  } else {
+    await page.getByLabel("Avoid doubling back").uncheck();
+    await expect(page.getByLabel("Avoid doubling back")).not.toBeChecked();
+    expectedToBeChecked = false;
+  }
 
   page = await loadInitialPage(browser);
   await page.getByRole("button", { name: "New route" }).click();
-  await expect(page.getByLabel("Avoid doubling back")).toBeChecked();
+  if (expectedToBeChecked) {
+    await expect(page.getByLabel("Avoid doubling back")).toBeChecked();
+  } else {
+    await expect(page.getByLabel("Avoid doubling back")).not.toBeChecked();
+  }
 });
