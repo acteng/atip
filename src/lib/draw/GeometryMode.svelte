acteng/atip

commit 7abd7a4ef1ca881911239e5b64e8b843c3adb3ea

RouteTool doesn't need to know Mode, and fix keyDown for split route

src/lib/draw/route/SplitRouteMode.svelte
@@ -146,7 +146,7 @@
   };
 
   // The escape key isn't registered at all for keypress, so use keydown
-  eventHandler.documentHandlers.onKeyDown = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keyDown = (e: KeyboardEvent) => {
     if (e.key == "Escape") {
       changeMode("edit-attribute");
       e.preventDefault();

src/lib/draw/route/route_tool.ts
@@ -12,7 +12,6 @@ import {
   overwritePolygonLayer,
   type FeatureWithProps,
 } from "../../../maplibre_helpers";
-import type { Mode } from "../../../types";
 import type { EventHandler } from "../event_handler";
 
 const source = "route-snapper";
@@ -21,7 +20,6 @@ const circleRadiusPixels = 10;
 const snapDistancePixels = 30;
 
 export class RouteTool {
-  mode: Mode;
   map: Map;
   inner: JsRouteSnapper;
   active: boolean;
@@ -29,7 +27,7 @@ export class RouteTool {
   eventListenersSuccessArea: ((f: FeatureWithProps<Polygon>) => void)[];
   eventListenersFailure: (() => void)[];
 
-  constructor(map: Map, graphBytes: Uint8Array, mode: Mode) {
+  constructor(map: Map, graphBytes: Uint8Array) {
     this.map = map;
     console.time("Deserialize and setup JsRouteSnapper");
     this.inner = new JsRouteSnapper(graphBytes);

src/lib/draw/route/RouteMode.svelte
@@ -44,7 +44,7 @@
     console.log(`Grabbing ${url}`);
     try {
       const graphBytes = await fetchWithProgress(url, progress);
-      routeTool = new RouteTool($map, graphBytes, "route");
+      routeTool = new RouteTool($map, graphBytes);
     } catch (err) {
       console.log(`Route tool broke: ${err}`);
       progress.innerHTML = "Failed to load";

commit 728315809020cf83c2ce19c91ed5d329632f37ed

Fix document key handlers broken in #220

src/lib/draw/GeometryMode.svelte
@@ -211,16 +211,16 @@
     polygonTool.onMouseUp();
   };
 
-  eventHandler.documentHandlers.keyPress = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keypress = (e: KeyboardEvent) => {
     routeTool.onKeyPress(e);
     polygonTool.onKeypress(e);
   };
 
-  eventHandler.documentHandlers.keyDown = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keydown = (e: KeyboardEvent) => {
     routeTool.onKeyDown(e);
   };
 
-  eventHandler.documentHandlers.keyUp = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keyup = (e: KeyboardEvent) => {
     routeTool.onKeyUp(e);
   };
 

src/lib/draw/StreetViewMode.svelte
@@ -64,7 +64,7 @@
     cursor = cursorFeature(e.lngLat.toArray());
   };
 
-  eventHandler.documentHandlers.keyDown = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keydown = (e: KeyboardEvent) => {
     if (e.key == "Escape") {
       changeMode("edit-attribute");
       e.preventDefault();

src/lib/draw/event_handler.ts
@@ -10,7 +10,7 @@ export const MapEvents = [
   "dragstart",
 ];
 
-export const DocumentEvents = ["keyPress", "keyUp", "keyDown"];
+export const DocumentEvents = ["keypress", "keyup", "keydown"];
 
 export class EventHandler {
   constructor() {
@@ -24,9 +24,9 @@ export class EventHandler {
       dragstart: (e: MapMouseEvent) => {},
     };
     this.documentHandlers = {
-      keyPress: (e: KeyboardEvent) => {},
-      keyUp: (e: KeyboardEvent) => {},
-      keyDown: (e: KeyboardEvent) => {},
+      keypress: (e: KeyboardEvent) => {},
+      keyup: (e: KeyboardEvent) => {},
+      keydown: (e: KeyboardEvent) => {},
     };
   }
 
@@ -41,8 +41,8 @@ export class EventHandler {
   };
 
   documentHandlers: {
-    keyPress: (e: KeyboardEvent) => void;
-    keyUp: (e: KeyboardEvent) => void;
-    keyDown: (e: KeyboardEvent) => void;
+    keypress: (e: KeyboardEvent) => void;
+    keyup: (e: KeyboardEvent) => void;
+    keydown: (e: KeyboardEvent) => void;
   };
 }

src/lib/draw/polygon/polygon_tool.ts
@@ -197,7 +197,7 @@ export class PolygonTool {
     eventHandler.mapHandlers.dblclick = this.onDoubleClick;
     eventHandler.mapHandlers.mousedown = this.onMouseDown;
     eventHandler.mapHandlers.mouseup = this.onMouseUp;
-    eventHandler.documentHandlers.keyPress = this.onKeypress;
+    eventHandler.documentHandlers.keypress = this.onKeypress;
   };
 
   addEventListenerSuccess(callback: (f: FeatureWithProps<Polygon>) => void) {

src/lib/draw/route/SplitRouteMode.svelte
@@ -144,7 +144,7 @@
   };
 
   // The escape key isn't registered at all for keypress, so use keydown
-  eventHandler.documentHandlers.keyDown = (e: KeyboardEvent) => {
+  eventHandler.documentHandlers.keydown = (e: KeyboardEvent) => {
     if (e.key == "Escape") {
       changeMode("edit-attribute");
       e.preventDefault();

src/lib/draw/route/route_tool.ts
@@ -340,9 +340,9 @@ export class RouteTool {
     eventHandler.mapHandlers.dblclick = this.onDoubleClick;
     eventHandler.mapHandlers.dragstart = this.onDragStart;
     eventHandler.mapHandlers.mouseup = this.onMouseUp;
-    eventHandler.documentHandlers.keyPress = this.onKeyPress;
-    eventHandler.documentHandlers.keyDown = this.onKeyDown;
-    eventHandler.documentHandlers.keyUp = this.onKeyUp;
+    eventHandler.documentHandlers.keypress = this.onKeyPress;
+    eventHandler.documentHandlers.keydown = this.onKeyDown;
+    eventHandler.documentHandlers.keyup = this.onKeyUp;
   };
 
   private redraw() {