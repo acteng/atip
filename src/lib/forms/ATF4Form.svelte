25a310b25c6dd0ad8351d69df1c8fed1a994eb64
Dustin Carlino <dabreegster@gmail.com>
2023-07-10 10:45:37 +0100

    Add a schema based on ATF4 guidance

---
+++ src/lib/forms/ATF4Form.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import schemaJson from "../../schemas/atf4.json";
+  import type { InterventionProps } from "../../types";
+  import AutogenerateForm from "./AutogenerateForm.svelte";
+  import type { Field } from "./types";
+
+  export let props: InterventionProps;
+
+  props.atf4 ||= {};
+
+  let x: Field = schemaJson;
+</script>
+
+<AutogenerateForm spec={x} bind:value={props.atf4} />
