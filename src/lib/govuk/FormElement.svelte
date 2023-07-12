5dd6b6967d49f9a682866cb099959d61b7e66ddf
Dustin Carlino <dabreegster@gmail.com>
2023-07-12 17:00:26 +0100

    Refactor usage of govuk so far

---
+++ src/lib/govuk/FormElement.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  // The label to show
+  export let label: string;
+  // A unique (per page) ID of the form element being labelled. Something in
+  // this component's slot must have this ID.
+  export let id: string;
+</script>
+
+<div class="govuk-form-group">
+  <label class="govuk-label" for={id}>{label}</label>
+  <slot />
+</div>
---
+++ src/lib/govuk/SecondaryButton.svelte
@@ -0,0 +1,9 @@
+<button
+  type="button"
+  class="govuk-button govuk-button--secondary"
+  data-module="govuk-button"
+  {...$$props}
+  on:click
+>
+  <slot />
+</button>
