1587ec07f9993130290aedc835095c86a705db27
Dustin Carlino <dabreegster@gmail.com>
2023-07-13 12:05:20 +0100

    Add some dropdown filters to the scheme browsing page

---
+++ src/lib/govuk/Select.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import FormElement from "../govuk/FormElement.svelte";
+
+  export let label: string;
+  // A unique (per page) ID
+  export let id: string;
+  // A list of [value, label] representing the choices
+  export let choices: [string, string][];
+  // Make the first option the empty string
+  export let emptyOption = true;
+
+  // The current value
+  export let value: string;
+</script>
+
+<FormElement {label} {id}>
+  <select class="govuk-select" {id} bind:value>
+    {#if emptyOption}
+      <option value="" />
+    {/if}
+
+    {#each choices as [thisValue, thisLabel]}
+      <option value={thisValue}>{thisLabel}</option>
+    {/each}
+  </select>
+</FormElement>
