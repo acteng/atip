86fc7cb0c79472c9f58ff66f6b05dd267aefb985
Dustin Carlino <dabreegster@gmail.com>
2023-07-23 14:03:03 +0100

    Add a similar hospitals layer

---
+++ src/lib/browse/HospitalsLayer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import {
+    overwritePmtilesSource,
+    overwritePolygonLayer,
+  } from "../../maplibre_helpers";
+  import { map } from "../../stores";
+  import { MapTooltips } from "../common";
+
+  let source = "hospitals";
+
+  overwritePmtilesSource(
+    $map,
+    source,
+    "https://atip.uk/layers/v1/hospitals.pmtiles"
+  );
+
+  overwritePolygonLayer($map, {
+    id: source,
+    source,
+    sourceLayer: source,
+    color: "blue",
+    opacity: 0.8,
+  });
+
+  function tooltip(props: { [name: string]: any }): string {
+    return props.name ?? "Unnamed hospital";
+  }
+</script>
+
+<MapTooltips layers={["hospitals"]} contents={tooltip} />
