9ffba0026836253d2782792670a4043c37b7e368
Dustin Carlino <dabreegster@gmail.com>
2023-07-23 13:44:48 +0100

    Add a schools layer to the browse scheme page, using pmtiles

---
+++ src/lib/browse/SchoolsLayer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import {
+    overwritePmtilesSource,
+    overwritePolygonLayer,
+  } from "../../maplibre_helpers";
+  import { map } from "../../stores";
+  import { MapTooltips } from "../common";
+
+  let source = "schools";
+
+  overwritePmtilesSource(
+    $map,
+    source,
+    "https://atip.uk/layers/v1/schools.pmtiles"
+  );
+
+  overwritePolygonLayer($map, {
+    id: source,
+    source,
+    sourceLayer: source,
+    color: "red",
+    opacity: 0.8,
+  });
+
+  function tooltip(props: { [name: string]: any }): string {
+    return props.name ?? "Unnamed school";
+  }
+</script>
+
+<MapTooltips layers={["schools"]} contents={tooltip} />
