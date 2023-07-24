5b451629b6c42da40156cb9bf02e4176f02362d0
Dustin Carlino <dabreegster@gmail.com>
2023-07-24 10:59:42 +0100

    Consolidate 3 layer-like controls

--- src/lib/Legend.svelte
+++ src/lib/Legend.svelte
@@ -6,54 +6,42 @@
   export let schema: Schema;
 </script>
 
-<div>
-  <CollapsibleCard label="Objects" open>
-    <ul>
-      {#if schema == "planning"}
-        <li>
-          <ColorLegend color={colors.preapp} />
-          Preapp
-        </li>
-        <li>
-          <ColorLegend color={colors.outline} />
-          Outline
-        </li>
-        <li>
-          <ColorLegend color={colors["reserved matters"]} />
-          Reserved matters
-        </li>
-        <li>
-          <ColorLegend color={colors["local plan"]} />
-          Local plan
-        </li>
-      {:else}
-        <li>
-          <ColorLegend color={colors.area} />
-          Areas
-        </li>
-        <li>
-          <ColorLegend color={colors.route} />
-          Routes
-        </li>
-        <li>
-          <ColorLegend color={colors.crossing} />
-          Crossings
-        </li>
-        <li>
-          <ColorLegend color={colors.other} />
-          Other
-        </li>
-      {/if}
-    </ul>
-  </CollapsibleCard>
-</div>
-
-<style>
-  div {
-    position: absolute;
-    left: 10px;
-    top: 60px;
-    background-color: white;
-    padding: 16px;
-  }
-</style>
+<CollapsibleCard label="Legend">
+  <ul>
+    {#if schema == "planning"}
+      <li>
+        <ColorLegend color={colors.preapp} />
+        Preapp
+      </li>
+      <li>
+        <ColorLegend color={colors.outline} />
+        Outline
+      </li>
+      <li>
+        <ColorLegend color={colors["reserved matters"]} />
+        Reserved matters
+      </li>
+      <li>
+        <ColorLegend color={colors["local plan"]} />
+        Local plan
+      </li>
+    {:else}
+      <li>
+        <ColorLegend color={colors.area} />
+        Areas
+      </li>
+      <li>
+        <ColorLegend color={colors.route} />
+        Routes
+      </li>
+      <li>
+        <ColorLegend color={colors.crossing} />
+        Crossings
+      </li>
+      <li>
+        <ColorLegend color={colors.other} />
+        Other
+      </li>
+    {/if}
+  </ul>
+</CollapsibleCard>
