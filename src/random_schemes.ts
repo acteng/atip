2bb56f70a4d63038449f1ac75ddf302356e15896
Dustin Carlino <dabreegster@gmail.com>
2023-07-26 15:25:19 +0100

    Generate random data for the scheme browse page

---
+++ src/random_schemes.ts
@@ -0,0 +1,119 @@
+// This script generates random sample data for input to the scheme browse page.
+// Note the random seed can't be set; this is non-deterministic.
+
+import * as fs from "fs";
+import turfBbox from "@turf/bbox";
+import booleanContains from "@turf/boolean-contains";
+import { lineString, point, polygon } from "@turf/helpers";
+import length from "@turf/length";
+import { randomPoint } from "@turf/random";
+import type {
+  Feature,
+  FeatureCollection,
+  LineString,
+  Polygon,
+  Position,
+} from "geojson";
+
+main();
+
+function main() {
+  let gj: FeatureCollection & { schemes: { [name: string]: any } } = {
+    type: "FeatureCollection",
+    features: [],
+    schemes: {},
+  };
+
+  let authorities = JSON.parse(
+    fs.readFileSync("assets/authorities.geojson", { encoding: "utf8" })
+  );
+
+  let count = 0;
+  for (let authority of authorities.features) {
+    let scheme_reference = `ATE${count}`;
+    count++;
+    gj.schemes[scheme_reference] = {
+      authority_or_region: authority.properties.name,
+      capital_scheme_id: count,
+      funding_programme: pickRandom(["ATF2", "ATF3", "ATF4"]),
+    };
+
+    // Make a few interventions for every authority
+
+    let pt = point(makePoints(authority.geometry, 1)[0]);
+    pt.properties!.scheme_reference = scheme_reference;
+    if (Math.random() < 0.5) {
+      pt.properties!.intervention_type = "crossing";
+      pt.properties!.name = pickRandom([
+        "New crossing",
+        "Upgrade existing crossing",
+      ]);
+      pt.properties!.description = pickRandom([
+        "Zebra",
+        "Toucan",
+        "Signalized",
+      ]);
+    } else {
+      pt.properties!.intervention_type = "other";
+      pt.properties!.name = pickRandom([
+        "Street lighting",
+        "Improve drainage",
+        "Repair damaged surface",
+      ]);
+    }
+    gj.features.push(pt);
+
+    let route = makeLineString(authority.geometry);
+    route.properties!.scheme_reference = scheme_reference;
+    route.properties!.intervention_type = "route";
+    route.properties!.length_meters =
+      length(route, { units: "kilometers" }) * 1000.0;
+    route.properties!.name = pickRandom(["School street", "Route from X to Y"]);
+    route.properties!.description = pickRandom([
+      "Bidirectional segregated",
+      "Shared use with segregation",
+      "Stepped cycle-track on the north side only",
+    ]);
+    gj.features.push(route);
+
+    let area = makePolygon(authority.geometry);
+    area.properties!.scheme_reference = scheme_reference;
+    area.properties!.intervention_type = "area";
+    area.properties!.name = pickRandom([
+      "Area-wide traffic management",
+      "Lighting improvements",
+      "New cycle hangars",
+    ]);
+    gj.features.push(area);
+  }
+
+  fs.writeFileSync("random_schemes.geojson", JSON.stringify(gj));
+}
+
+function makePoints(boundary: Feature<Polygon>, count: number): Position[] {
+  let points = [];
+  let bbox = turfBbox(boundary);
+  while (points.length < count) {
+    let pt = randomPoint(1, { bbox }).features[0];
+    if (booleanContains(boundary, pt)) {
+      points.push(pt.geometry.coordinates);
+    }
+  }
+  return points;
+}
+
+function makeLineString(boundary: Feature<Polygon>): Feature<LineString> {
+  // turf's randomLineString picks really tiny lengths
+  let points = makePoints(boundary, 2);
+  return lineString([points[0], points[1]]);
+}
+
+function makePolygon(boundary: Feature<Polygon>): Feature<Polygon> {
+  // turf's randomPolygon produces tiny areas
+  let points = makePoints(boundary, 3);
+  return polygon([[points[0], points[1], points[2], points[0]]]);
+}
+
+function pickRandom<T>(items: T[]): T {
+  return items[Math.floor(Math.random() * items.length)];
+}
