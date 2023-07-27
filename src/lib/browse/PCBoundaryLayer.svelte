bc8fb26461c53d2faf84811d895950f43d0142e2
Dustin Carlino <dabreegster@gmail.com>
2023-07-27 17:41:34 +0100

    Add a layer for parliamentary constituencies

---
+++ src/lib/browse/PCBoundaryLayer.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import type { MapGeoJSONFeature } from "maplibre-gl";
+  import {
+    overwriteLineLayer,
+    overwritePmtilesSource,
+    overwritePolygonLayer,
+  } from "../../maplibre_helpers";
+  import { map } from "../../stores";
+  import {
+    ColorLegend,
+    ExternalLink,
+    HelpButton,
+    InteractiveLayer,
+  } from "../common";
+  import { Checkbox } from "../govuk";
+  import { colors } from "./colors";
+
+  let name = "parliamentary_constituencies";
+  let color = colors.parliamentary_constituencies;
+  let outlineLayer = `${name}-outline`;
+
+  overwritePmtilesSource(
+    $map,
+    name,
+    `https://atip.uk/layers/v1/${name}.pmtiles`
+  );
+
+  overwritePolygonLayer($map, {
+    id: name,
+    source: name,
+    sourceLayer: name,
+    color,
+    opacity: ["case", ["boolean", ["feature-state", "hover"], false], 0.5, 0.1],
+  });
+  overwriteLineLayer($map, {
+    id: outlineLayer,
+    source: name,
+    sourceLayer: name,
+    color,
+    width: 5,
+  });
+
+  let show = false;
+  // InteractiveLayer manages the polygon layer, but we also need to control the outline
+  $: {
+    if ($map.getLayer(outlineLayer)) {
+      $map.setLayoutProperty(
+        outlineLayer,
+        "visibility",
+        show ? "visible" : "none"
+      );
+    }
+  }
+
+  function tooltip(feature: MapGeoJSONFeature): string {
+    return `<p>${feature.properties.Name} (${feature.properties.Census_Code})</p>`;
+  }
+
+  function onClick(e: CustomEvent<MapGeoJSONFeature>) {
+    window.open(
+      `https://www.ons.gov.uk/visualisations/areas/${e.detail.properties.Census_Code}`,
+      "_blank"
+    );
+  }
+</script>
+
+<Checkbox id={name} bind:checked={show}>
+  <ColorLegend {color} />
+  Parliamentary constituencies
+  <span slot="right">
+    <HelpButton>
+      <p>
+        Data from <ExternalLink
+          href="https://www.ordnancesurvey.co.uk/products/boundary-line"
+        >
+          Ordnance Survey Boundary-Line
+        </ExternalLink>.
+      </p>
+      <p>
+        License: <ExternalLink
+          href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+        >
+          Open Government License
+        </ExternalLink>
+      </p>
+    </HelpButton>
+  </span>
+</Checkbox>
+
+<InteractiveLayer layer={name} {tooltip} {show} on:click={onClick} />
--- src/lib/browse/colors.ts
+++ src/lib/browse/colors.ts
@@ -3,6 +3,7 @@ export const colors = {
   schools: "#007DBB",
   hospitals: "#B73D25",
   mrn: "#006478",
+  parliamentary_constituencies: "#006E59",
 
   atf2: "#00AFFF",
   atf3: "#FF62DC",
