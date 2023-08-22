ba97d045e884f66fd77a17c674110d5f60913fbc
Dustin Carlino <dabreegster@gmail.com>
2023-08-22 13:19:09 +0100

    Include paths in the Streetview highlight. #283

--- src/lib/maplibre/layer_helper_utils.ts
+++ src/lib/maplibre/layer_helper_utils.ts
@@ -2,37 +2,47 @@ import { get } from "svelte/store";
 import { map as mapStore } from "../../stores";
 import { LayerHelper } from "./layer_helper";
 
-// These depend on MapTiler basemap styles
-export const roadLayerIds = [
-  "road_minor",
-  "road_secondary_tertiary",
-  "road_motorway",
-  "road_trunk_primary",
-  "road_link",
-  "road_motorway_link",
-  "road_trunk_primary_link",
-  "road_minor",
-  "bridge_street",
-  "bridge_minor",
-  "bridge_secondary_tertiary",
-  "bridge_trunk_primary",
-  "bridge_motorway",
-  "tunnel_minor",
-  "tunnel_secondary_tertiary",
-  "tunnel_trunk_primary",
-  "tunnel_motorway",
-  "road_service_track",
-  "Road network",
-];
+// Each MapTiler basemap style uses different layer IDs for roads and paths
 
-export const getRoadLayerHelpers: () => Array<LayerHelper> = () => {
-  const mapValue = get(mapStore);
-  return mapValue
+const datavizLayers = ["Road network", "Path"];
+
+function streetsLayers() {
+  let layers = [];
+  for (let outer of ["road", "bridge", "tunnel"]) {
+    for (let inner of [
+      "link",
+      "minor",
+      "minor_construction",
+      "motorway",
+      "motorway_construction",
+      "motorway_link",
+      "path_pedestrian",
+      "secondary_tertiary",
+      "secondary_tertiary_construction",
+      "service_track",
+      "service_track_construction",
+      "street",
+      "trunk_primary",
+      "trunk_primary_construction",
+      "trunk_primary_link",
+    ]) {
+      layers.push(`${outer}_${inner}`);
+    }
+  }
+  return layers;
+}
+
+const satelliteLayers = ["Path", "Road", "Tunnel"];
+
+const roadLayerIds = datavizLayers.concat(streetsLayers(), satelliteLayers);
+
+export function getRoadLayerHelpers(): LayerHelper[] {
+  return get(mapStore)
     .getStyle()
-    .layers.filter((layer: any) => {
+    .layers.filter((layer) => {
       return roadLayerIds.includes(layer.id);
     })
     .map((layer) => {
       return new LayerHelper(layer);
     });
-};
+}
