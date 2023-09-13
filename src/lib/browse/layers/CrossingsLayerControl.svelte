4af3a9c2ee6a32ba687795b4212e59337d202a18
Peter York <peter.york@dft.gov.uk>
2023-09-13 15:11:52 +0100

    Initial crossings commit

---
+++ src/lib/browse/layers/CrossingsLayerControl.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { circleRadius } from "colors";
+  import { ExternalLink } from "lib/common";
+  import PointAmenityLayerControl from "./PointAmenityLayerControl.svelte";
+
+  const name = "crossings";
+</script>
+
+<PointAmenityLayerControl
+  {name}
+  singularNoun="crossing"
+  pluralNoun="Crossings"
+  {circleRadius}
+>
+  <p>
+    This shows <ExternalLink
+      href="https://wiki.openstreetmap.org/wiki/Tag:crossing"
+    >
+      railway station
+    </ExternalLink> data from OpenStreetMap (as of 9 August 2023).
+  </p>
+
+  <p>
+    License: <ExternalLink href="https://www.openstreetmap.org/copyright">
+      Open Data Commons Open Database License
+    </ExternalLink>
+  </p>
+</PointAmenityLayerControl>
