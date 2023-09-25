3675ff8f9d1784e26e7f1a1b0378a997785cad3d
Peter York <peter.york@dft.gov.uk>
2023-09-25 15:25:58 +0100

    Begin Line measuring component

---
+++ src/lib/common/LineMeasureController.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { SecondaryButton } from "lib/govuk";
+  import { LngLat, MapMouseEvent } from "maplibre-gl";
+  import { CollapsibleCard } from ".";
+
+  export let isInactive = true;
+  const waypoints: LngLat[] = [];
+
+  export function handleMapClickEvent(e: MapMouseEvent) {
+    if (isInactive) {
+      return;
+    }
+
+    if (e.type === "click") {
+    }
+
+    if (e.type === "dblclick") {
+    }
+  }
+
+  export function enableStreetView() {
+    isInactive = false;
+  }
+
+  export function disableStreetView() {
+    isInactive = true;
+  }
+</script>
+
+{#if isInactive}
+  <SecondaryButton on:click={enableStreetView}>
+    Enable Line Measurement Tool
+  </SecondaryButton>
+{:else}
+  <SecondaryButton on:click={disableStreetView}>
+    Disable Line Measurement Tool
+  </SecondaryButton>
+{/if}
+{#if !isInactive}
+  <CollapsibleCard label="Help">
+    <ul>
+      <li>
+        <b>Click</b>
+        on the map to place a new waypoint
+      </li>
+      <li>
+        <b>Click & Drag</b>
+        an existing waypoint to move it
+      </li>
+      <li>
+        <b>Right click</b>
+        an existing waypoint to delete it
+      </li>
+      <li>
+        Press <b>Escape</b>
+        to exit this mode
+      </li>
+    </ul>
+  </CollapsibleCard>
+{/if}
