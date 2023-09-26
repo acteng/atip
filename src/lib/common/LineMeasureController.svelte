43ff5adb491886ef47722e2a1db4e6ed95d874e7
Peter York <peter.york@dft.gov.uk>
2023-09-26 11:46:10 +0100

    Removable pins on the browse page. This will break critical currently

--- src/lib/common/LineMeasureController.svelte
+++ src/lib/common/LineMeasureController.svelte
@@ -1,38 +1,99 @@
 <script lang="ts">
+  import Pin from "lib/critical_entry/Pin.svelte";
   import { SecondaryButton } from "lib/govuk";
   import { LngLat, MapMouseEvent } from "maplibre-gl";
+  import { map } from "stores";
+  import { onDestroy, onMount } from "svelte";
   import { CollapsibleCard } from ".";
 
-  export let isInactive = true;
-  const waypoints: LngLat[] = [];
+  let isInactive = true;
+  let waypoints: LngLat[] = [];
+  let isShiftDown = false;
 
-  export function handleMapClickEvent(e: MapMouseEvent) {
+  function handleMapClickEvent(e: MapMouseEvent) {
     if (isInactive) {
       return;
     }
 
-    if (e.type === "click") {
+    if (!isShiftDown) {
+      waypoints.push(e.lngLat);
+      waypoints = waypoints;
     }
-
-    if (e.type === "dblclick") {
+    if (isShiftDown && waypoints.length > 0) {
+      console.log("removing?");
+      findAndRemoveNeareastWaypoint(e.lngLat);
     }
   }
 
-  export function enableStreetView() {
+  function findAndRemoveNeareastWaypoint(clickLocation: LngLat) {
+    const maxLngLatDistance = new LngLat(0, -90).distanceTo(new LngLat(0, 90));
+    let currentShortestDistance = maxLngLatDistance;
+    let waypointToRemove: LngLat | undefined = undefined;
+
+    console.log(`clickLocation ${clickLocation.lat}, ${clickLocation.lng}`);
+    console.log(waypoints)
+    
+
+    waypoints.forEach((waypoint) => {
+      if (currentShortestDistance >= clickLocation.distanceTo(waypoint)) {
+        waypointToRemove = waypoint;
+        currentShortestDistance = clickLocation.distanceTo(waypoint);
+        console.log(
+          `waypoint ${waypoint}, ${clickLocation.distanceTo(
+            waypoint
+          )}, prev shorted ${currentShortestDistance}`
+        );
+      }
+    });
+
+    console.log(waypointToRemove)
+    if (waypointToRemove) {
+      waypoints = waypoints.filter((waypoint) => {
+        return (
+          waypoint.lng != waypointToRemove?.lng ||
+          waypoint.lat != waypointToRemove?.lat
+        );
+      });
+    }
+    console.log(waypoints);
+    waypoints = waypoints;
+  }
+
+  function enableMeasurement() {
     isInactive = false;
+    $map.on("click", handleMapClickEvent);
   }
 
-  export function disableStreetView() {
+  function disableMeasurement() {
     isInactive = true;
+    $map.off("click", handleMapClickEvent);
   }
+
+  function keyDown(keyDownEvent: KeyboardEvent) {
+    if (keyDownEvent.key === "Shift") isShiftDown = true;
+  }
+
+  function keyUp(keyUpEvent: KeyboardEvent) {
+    if (keyUpEvent.key === "Shift") isShiftDown = false;
+  }
+
+  onMount(() => {
+    document.addEventListener("keydown", keyDown);
+    document.addEventListener("keyup", keyUp);
+  });
+
+  onDestroy(() => {
+    document.removeEventListener("keydown", keyDown);
+    document.removeEventListener("keyup", keyUp);
+  });
 </script>
 
 {#if isInactive}
-  <SecondaryButton on:click={enableStreetView}>
+  <SecondaryButton on:click={enableMeasurement}>
     Enable Line Measurement Tool
   </SecondaryButton>
 {:else}
-  <SecondaryButton on:click={disableStreetView}>
+  <SecondaryButton on:click={disableMeasurement}>
     Disable Line Measurement Tool
   </SecondaryButton>
 {/if}
@@ -48,7 +109,7 @@
         an existing waypoint to move it
       </li>
       <li>
-        <b>Right click</b>
+        <b>Double click</b>
         an existing waypoint to delete it
       </li>
       <li>
@@ -57,4 +118,7 @@
       </li>
     </ul>
   </CollapsibleCard>
+  {#each waypoints as waypoint}
+    <Pin bind:markerPosition={waypoint} />
+  {/each}
 {/if}
--- src/lib/critical_entry/Pin.svelte
+++ src/lib/critical_entry/Pin.svelte
@@ -1,32 +1,16 @@
 <script lang="ts">
-  import { Marker, type LngLat, type MapMouseEvent } from "maplibre-gl";
+  import { Marker, type LngLat } from "maplibre-gl";
   import { map } from "stores";
   import { onDestroy, onMount } from "svelte";
 
-  export let enableAdding: boolean;
-
   let marker: Marker | null = null;
-  export let markerPosition: LngLat | null = null;
+  export let markerPosition: LngLat;
 
   onMount(() => {
-    $map.on("click", startMarker);
     // TODO If the user toggles streetview, this cursor is lost
-    $map.getCanvas().style.cursor = "crosshair";
-  });
-  onDestroy(() => {
-    if (marker) {
-      marker.remove();
-    }
-    $map.off("click", startMarker);
-  });
-
-  function startMarker(e: MapMouseEvent) {
-    if (marker || !enableAdding) {
-      return;
-    }
-    $map.getCanvas().style.cursor = "inherit";
-    marker = new Marker({ draggable: true }).setLngLat(e.lngLat).addTo($map);
-    markerPosition = marker.getLngLat();
+    marker = new Marker({ draggable: true })
+      .setLngLat(markerPosition)
+      .addTo($map);
     marker.on("dragstart", () => {
       $map.getCanvas().style.cursor = "grabbing";
     });
@@ -34,6 +18,13 @@
       $map.getCanvas().style.cursor = "inherit";
       markerPosition = marker!.getLngLat();
     });
-    // TODO Change cursor when we're hovering on the marker
-  }
+    marker.on("dblclick", () => {
+      console.log("you clicked me");
+    });
+  });
+  onDestroy(() => {
+    if (marker) {
+      marker.remove();
+    }
+  });
 </script>
