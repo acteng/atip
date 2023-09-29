8fbe48f96786433f9e1c67787e594b569ec102e0
Dustin Carlino <dabreegster@gmail.com>
2023-09-29 10:51:07 +0100

    Criticals. Cluster radius is acting weird, but otherwise the popup-on-click is much nicer

--- src/lib/browse/layers/points/CriticalIssues.svelte
+++ src/lib/browse/layers/points/CriticalIssues.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-  import { CollapsibleCard, ColorLegend, InteractiveLayer } from "lib/common";
+  import { CollapsibleCard, ColorLegend } from "lib/common";
   import {
     Checkbox,
     CheckboxGroup,
     ErrorMessage,
     FormElement,
   } from "lib/govuk";
-  import {
-    cleanupSource,
-    emptyGeojson,
-    overwriteCircleLayer,
-  } from "lib/maplibre";
-  import type { GeoJSONSource, MapGeoJSONFeature } from "maplibre-gl";
+  import { emptyGeojson } from "lib/maplibre";
   import { map } from "stores";
+  import {
+    CircleLayer,
+    GeoJSON,
+    Popup,
+    SymbolLayer,
+    type LayerClickInfo,
+  } from "svelte-maplibre";
   import { parseCriticalIssuesExcel } from "../../data";
 
   let fileInput: HTMLInputElement;
@@ -20,65 +22,15 @@
   let source = "criticals";
   let color = "red";
   let show = true;
+  $: visibility = show ? "visible" : "none";
   let numberIssues = 0;
   let errorMessage = "";
 
-  cleanupSource($map, source);
-  $map.addSource(source, {
-    type: "geojson",
-    data: emptyGeojson(),
-    cluster: true,
-    clusterMaxZoom: 14,
-    clusterRadius: 50,
-  });
-
-  // Render clusters
-  overwriteCircleLayer($map, {
-    id: `${source}-clusters`,
-    source,
-    filter: ["has", "point_count"],
-    color,
-    opacity: 0.9,
-    // 20 pixels for clusters with < 15 points, 30 pixels for < 30 points, and 40 pixels for more
-    radius: ["step", ["get", "point_count"], 20, 15, 30, 30, 40],
-  });
-  $map.addLayer({
-    id: `${source}-counts`,
-    source,
-    type: "symbol",
-    filter: ["has", "point_count"],
-    layout: {
-      "text-field": "{point_count_abbreviated}",
-    },
-  });
-
-  // Render individual criticals
-  overwriteCircleLayer($map, {
-    id: `${source}-points`,
-    source,
-    filter: ["!", ["has", "point_count"]],
-    color,
-    opacity: 0.9,
-    radius: 15,
-    strokeColor: "black",
-    strokeWidth: 3,
-  });
-
-  // InteractiveLayers don't manage one layer
-  $: {
-    if ($map.getLayer(`${source}-counts`)) {
-      $map.setLayoutProperty(
-        `${source}-counts`,
-        "visibility",
-        show ? "visible" : "none"
-      );
-    }
-  }
+  let gj = emptyGeojson();
 
   async function onChange(e: Event) {
     try {
-      let gj = await parseCriticalIssuesExcel(fileInput.files![0]);
-      ($map.getSource(source) as GeoJSONSource).setData(gj);
+      gj = await parseCriticalIssuesExcel(fileInput.files![0]);
       numberIssues = gj.features.length;
       errorMessage = "";
     } catch (err) {
@@ -86,29 +38,13 @@
     }
   }
 
-  function tooltip(feature: MapGeoJSONFeature): string {
-    let x = `<div style="max-width: 30vw;">`;
-    x += `<h2>${feature.properties.critical_type}</h2>`;
-    x += `<p><u>Location</u>: ${feature.properties.location_description}</p>`;
-    x += `<p><u>Notes</u>: ${feature.properties.notes}</p>`;
-    x += `<hr />`;
-    x += `<p>Critical issue ID: <b>${feature.properties.id}</b></p>`;
-    x += `<p>Submitted by <b>${
-      feature.properties.inspector ?? "???"
-    }</b> at <b>${feature.properties.submission_time}</b></p>`;
-    x += `<p>For scheme <b>${feature.properties.scheme_reference}</b> in stage <b>${feature.properties.current_design_stage}</b></p>`;
-    x += `</div>`;
-    return x;
-  }
-
-  function clickCluster(e: CustomEvent<MapGeoJSONFeature>) {
+  function clickCluster(e: CustomEvent<LayerClickInfo>) {
     ($map.getSource(source) as GeoJSONSource).getClusterExpansionZoom(
-      e.detail.properties.cluster_id,
+      e.detail.features[0].properties.cluster_id,
       (err, zoom) => {
         if (!err) {
           $map.easeTo({
-            // @ts-ignore Something's wrong with MapGeoJSONFeature types
-            center: e.detail.geometry.coordinates,
+            center: e.detail.features[0].geometry.coordinates,
             zoom: zoom as number,
           });
         }
@@ -138,16 +74,74 @@
   {/if}
 </CollapsibleCard>
 
-<InteractiveLayer
-  layer={source + "-points"}
-  {tooltip}
-  {show}
-  clickable={false}
-/>
-<InteractiveLayer
-  layer={source + "-clusters"}
-  {show}
-  clickable
-  on:click={clickCluster}
-  ignoreHigherLayers={[source + "-counts"]}
-/>
+<GeoJSON
+  id={source}
+  data={gj}
+  cluster={{
+    // TODO Something's weird
+    radius: 500,
+    maxZoom: 14,
+  }}
+>
+  <CircleLayer
+    id={`${source}-clusters`}
+    applyToClusters
+    paint={{
+      "circle-color": color,
+      "circle-opacity": 0.9,
+      // 20 pixels for clusters with < 15 points, 30 pixels for < 30 points, and 40 pixels for more
+      "circle-radius": ["step", ["get", "point_count"], 20, 15, 30, 30, 40],
+    }}
+    layout={{ visibility }}
+    hoverCursor="pointer"
+    on:click={clickCluster}
+  />
+  <SymbolLayer
+    applyToClusters
+    layout={{
+      "text-field": "{point_count_abbreviated}",
+      visibility,
+    }}
+  />
+  <CircleLayer
+    id={`${source}-counts`}
+    applyToClusters={false}
+    paint={{
+      "circle-color": color,
+      "circle-opacity": 0.9,
+      "circle-radius": 15,
+      "circle-stroke-color": "black",
+      "circle-stroke-width": 3,
+    }}
+    layout={{ visibility }}
+  >
+    <Popup openOn="click" let:features>
+      {@const props = features[0].properties}
+      <div style="max-width: 30vw;">
+        <h2>{props.critical_type}</h2>
+        <p>
+          <u>Location</u>
+          : {props.location_description}
+        </p>
+        <p>
+          <u>Notes</u>
+          : {props.notes}
+        </p>
+        <hr />
+        <p>
+          Critical issue ID: <b>{props.id}</b>
+        </p>
+        <p>
+          Submitted by <b>{props.inspector ?? "???"}</b>
+          at
+          <b>{props.submission_time}</b>
+        </p>
+        <p>
+          For scheme <b>{props.scheme_reference}</b>
+          in stage
+          <b>{props.current_design_stage}</b>
+        </p>
+      </div>
+    </Popup>
+  </CircleLayer>
+</GeoJSON>
