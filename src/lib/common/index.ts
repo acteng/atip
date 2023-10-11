27f43ae38bdf7ab8775297713d9c5d313647c4de
Dustin Carlino <dabreegster@gmail.com>
2023-10-11 13:27:42 +0100

    Add a geocoder using MapTiler

--- src/lib/common/index.ts
+++ src/lib/common/index.ts
@@ -7,6 +7,7 @@ export { default as ColorLegend } from "./ColorLegend.svelte";
 export { default as DiscreteLegend } from "./DiscreteLegend.svelte";
 export { default as ExternalLink } from "./ExternalLink.svelte";
 export { default as FileInput } from "./FileInput.svelte";
+export { default as Geocoder } from "./Geocoder.svelte";
 export { default as HelpButton } from "./HelpButton.svelte";
 export { default as Layout } from "./Layout.svelte";
 export { default as Legend } from "./Legend.svelte";
