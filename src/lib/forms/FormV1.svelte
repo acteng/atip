7ba26bce4d19d268235de919613e199f7fe94721
Dustin Carlino <dabreegster@gmail.com>
2023-10-19 15:35:34 +0100

    Simplify form v1 props plumbing

--- src/lib/forms/FormV1.svelte
+++ src/lib/forms/FormV1.svelte
@@ -1,26 +1,17 @@
 <script lang="ts">
-  import type { LineString } from "geojson";
   import { FormElement, Radio, SecondaryButton, TextArea } from "lib/govuk";
   import { prettyPrintMeters } from "lib/maplibre";
-  import { gjScheme, routeTool } from "stores";
-  import type { Feature } from "types";
+  import { routeTool } from "stores";
+  import type { InterventionProps } from "types";
 
   export let id: number;
-  export let name: string;
-  export let intervention_type: "area" | "route" | "crossing" | "other";
-  export let description: string;
-  export let length_meters: number | undefined;
+  export let props: InterventionProps;
 
   // Sets the intervention name to "From {road1 and road2} to {road3 and
   // road4}". Only meant to be useful for routes currently.
   function autoFillName() {
-    let feature = $gjScheme.features.find(
-      (f) => f.id == id
-    ) as Feature<LineString>;
     try {
-      name = $routeTool!.inner.routeNameForWaypoints(
-        feature.properties.waypoints
-      );
+      props.name = $routeTool!.inner.routeNameForWaypoints(props.waypoints);
     } catch (e) {
       window.alert(`Couldn't auto-name route: ${e}`);
     }
@@ -28,9 +19,9 @@
 </script>
 
 <FormElement label="Name" id={"name-" + id}>
-  <input type="text" class="govuk-input" bind:value={name} />
+  <input type="text" class="govuk-input" bind:value={props.name} />
   <!-- Only LineStrings can be auto-named, and length_meters being set is the simplest proxy for that -->
-  {#if length_meters}
+  {#if props.length_meters}
     <SecondaryButton on:click={() => autoFillName()} disabled={!$routeTool}>
       Auto-fill
     </SecondaryButton>
@@ -47,11 +38,11 @@
     ["other", "Other"],
   ]}
   inlineSmall
-  bind:value={intervention_type}
+  bind:value={props.intervention_type}
 />
 
-<TextArea label="Description" bind:value={description} />
+<TextArea label="Description" bind:value={props.description} />
 
-{#if length_meters}
-  <p>Length: {prettyPrintMeters(length_meters)}</p>
+{#if props.length_meters}
+  <p>Length: {prettyPrintMeters(props.length_meters)}</p>
 {/if}
