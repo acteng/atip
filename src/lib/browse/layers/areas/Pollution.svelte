c101ee96d0cb0352470909a138ac5d4a6466e849
Dustin Carlino <dabreegster@gmail.com>
2023-10-25 14:19:06 +0100

    Clean up a bit, try to style legend

--- src/lib/browse/layers/areas/Pollution.svelte
+++ src/lib/browse/layers/areas/Pollution.svelte
@@ -7,30 +7,23 @@
 
   let show = false;
   let opacity = 50;
-
   let pollutant = "PM25_viridis";
 
   // TODO Corrected for natural sources or not?
 
-  // TODO Combo em, also with a title?
-  $: urlBase = {
-    NOx_viridis: "aq_amb_2021",
-    NOxRoads_viridis: "aq_amb_2022",
-    PM25_viridis: "aq_amb_2021",
-    PM25Roads_viridis: "aq_amb_2022",
-    PM10_viridis: "aq_amb_2021",
-    PM10Roads_viridis: "aq_amb_2022",
+  // URLs and layers found from https://uk-air.defra.gov.uk/data/wms-services
+  $: info = {
+    NOx_viridis: ["aq_amb_2021", "21"],
+    PM25_viridis: ["aq_amb_2021", "20"],
+    PM10_viridis: ["aq_amb_2021", "21"],
+    NOxRoads_viridis: ["aq_amb_2022", "22"],
+    PM25Roads_viridis: ["aq_amb_2022", "14"],
+    PM10Roads_viridis: ["aq_amb_2022", "21"],
   }[pollutant];
-  $: yearLayer = {
-    NOx_viridis: "21",
-    NOxRoads_viridis: "22",
-    PM25_viridis: "20",
-    PM25Roads_viridis: "14",
-    PM10_viridis: "21",
-    PM10Roads_viridis: "21",
-  }[pollutant];
+  $: wmsUrl = `https://ukair.maps.rcdo.co.uk/ukairserver/services/${info[0]}/${pollutant}/MapServer/WMSServer`;
+  $: yearLayer = info[1];
 
-  function tilesUrl(pollutant: string): string {
+  function tilesUrl(wmsUrl: string): string {
     let params = new URLSearchParams({
       request: "GetMap",
       version: "1.3.0",
@@ -42,11 +35,10 @@
       layers: yearLayer,
     }).toString();
     // Don't escape the {} in the bbox, so specify it manually below
-    return `https://ukair.maps.rcdo.co.uk/ukairserver/services/${urlBase}/${pollutant}/MapServer/WMSServer?bbox={bbox-epsg-3857}&${params}`;
+    return `${wmsUrl}?bbox={bbox-epsg-3857}&${params}`;
   }
 
-  // TODO Refactor
-  function legendUrl(pollutant: string): string {
+  function legendUrl(wmsUrl: string): string {
     let params = new URLSearchParams({
       request: "GetLegendGraphic",
       version: "1.3.0",
@@ -54,7 +46,7 @@
       // Not plural here
       layer: yearLayer,
     }).toString();
-    return `https://ukair.maps.rcdo.co.uk/ukairserver/services/${urlBase}/${pollutant}/MapServer/WMSServer?${params}`;
+    return `${wmsUrl}?${params}`;
   }
 </script>
 
@@ -73,18 +65,19 @@
     label="Pollutant"
     id="pollutant"
     choices={[
-      ["NOx_viridis", "Background NOx (Oxides of nitrogen)"],
-      ["NOxRoads_viridis", "Roadside NOx (Oxides of nitrogen)"],
-      ["PM25_viridis", "Background PM2.5 (Particulate matter < 2.5 microns)"],
+      ["PM25_viridis", "Background PM2.5"],
+      ["PM10_viridis", "Background PM10"],
+      ["NOx_viridis", "Background NOx"],
       [
         "PM25Roads_viridis",
-        "Roadside PM2.5 (Particulate matter < 2.5 microns)",
+        "Roadside PM2.5"
       ],
-      ["PM10_viridis", "Background PM10 (Particulate matter < 10 microns)"],
-      ["PM10Roads_viridis", "Roadside PM10 (Particulate matter < 10 microns)"],
+      ["PM10Roads_viridis", "Roadside PM10"],
+      ["NOxRoads_viridis", "Roadside NOx"],
     ]}
     bind:value={pollutant}
   />
+  <p>Data for <b>{yearLayer}</b></p>
 
   <div>
     <label>
@@ -93,10 +86,10 @@
     </label>
   </div>
 
-  <img src={legendUrl(pollutant)} />
+  <img src={legendUrl(wmsUrl)} width={150} alt={`Legend for ${pollutant} layer`} />
 {/if}
 
-<RasterTileSource tiles={[tilesUrl(pollutant)]} tileSize={256}>
+<RasterTileSource tiles={[tilesUrl(wmsUrl)]} tileSize={256}>
   <RasterLayer
     {...layerId("pollution")}
     paint={{
