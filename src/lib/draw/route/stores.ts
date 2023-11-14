ba2ba01e6815beb54f3511357cba57860df57a8e
Dustin Carlino <dabreegster@gmail.com>
2023-11-14 09:01:45 +0000

    Move the route tool layer to Svelte

--- src/lib/draw/route/stores.ts
+++ src/lib/draw/route/stores.ts
@@ -1,9 +1,11 @@
 import type { FeatureCollection } from "@maptiler/geocoding-control/types";
+import type { GeoJSON } from "geojson";
 import { emptyGeojson } from "lib/maplibre";
 import { writable, type Writable } from "svelte/store";
 
 // These are necessary to communicate between components nested under the sidebar and map
 
+export const routeToolGj: Writable<GeoJSON> = writable(emptyGeojson());
 export const geocoderGj: Writable<FeatureCollection> = writable(
   emptyGeojson() as FeatureCollection
 );
