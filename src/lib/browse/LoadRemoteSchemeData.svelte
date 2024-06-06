9f0f5fb6e58baa7828c11aeb8906b5a60170d9d2
Dustin Carlino <dabreegster@gmail.com>
2024-06-06 15:40:26 +0100

    Skip loading scheme data by a URL param

--- src/lib/browse/LoadRemoteSchemeData.svelte
+++ src/lib/browse/LoadRemoteSchemeData.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import { SecondaryButton } from "govuk-svelte";
   import { fetchWithProgress, privateResourceBaseUrl } from "lib/common";
   // means setProgress may get percentages over 100.
   import { onMount } from "svelte";
 
   export let loadFile: (text: string) => void;
 
+  let shouldLoad = !new URLSearchParams(window.location.search).has(
+    "disable_schemes",
+  );
   let loading = true;
   let progress = 0;
 
-  onMount(async () => {
+  async function loadData() {
     let bytes = await fetchWithProgress(
       `${privateResourceBaseUrl()}/v1/all_schemes_output.geojson.gz`,
       (p) => {
@@ -19,10 +23,25 @@
     let text = new TextDecoder().decode(bytes);
     loadFile(text);
     loading = false;
+  }
+
+  onMount(async () => {
+    if (shouldLoad) {
+      await loadData();
+    }
   });
+
+  async function loadDataManually() {
+    shouldLoad = true;
+    await loadData();
+  }
 </script>
 
-{#if loading}
+{#if !shouldLoad}
+  <SecondaryButton on:click={loadDataManually}>
+    Load latest scheme data
+  </SecondaryButton>
+{:else if loading}
   {#if progress <= 100}
     <label>
       Downloading scheme data
