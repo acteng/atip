58f0fea7b3e375e6f4d37b2ed9363991fa34e7d6
Dustin Carlino <dabreegster@gmail.com>
2024-10-02 15:27:44 +0100

    Organize scheme-related components properly

--- src/lib/browse/colors.ts
+++ src/lib/browse/colors.ts
@@ -1,6 +1,4 @@
-import { constructMatchExpression } from "lib/maplibre";
-import { type DataDrivenPropertyValueSpecification } from "maplibre-gl";
-import { fundingProgrammesForColouringAndFiltering } from "./data";
+import type { DataDrivenPropertyValueSpecification } from "maplibre-gl";
 
 // These are specific to the browse page
 export const colors = {
@@ -89,65 +87,3 @@ export const denseLineWidth: DataDrivenPropertyValueSpecification<number> = [
   18,
   20.0,
 ];
-
-export function styleByFundingProgramme(): [
-  DataDrivenPropertyValueSpecification<string>,
-  [string, string][],
-] {
-  let [colorMapping, legendRows] = getColorMappingAndLegend(
-    fundingProgrammesForColouringAndFiltering,
-    colors.funding_programmes,
-  );
-
-  let color = constructMatchExpression(
-    ["get", "funding_programme"],
-    colorMapping,
-    "grey",
-  );
-  return [color, legendRows];
-}
-
-export function styleByCurrentMilestone(): [
-  DataDrivenPropertyValueSpecification<string>,
-  [string, string][],
-] {
-  let stageGates = [
-    "removed",
-    "no data",
-    "not progressed",
-    "superseded",
-    "preliminary design completed",
-    "feasability design completed",
-    "detailed design completed",
-    "consruction started",
-    "construction completed",
-  ];
-
-  let [colorMapping, legendRows] = getColorMappingAndLegend(
-    stageGates,
-    colors.current_milestone,
-  );
-
-  let color = constructMatchExpression(
-    ["get", "current_milestone"],
-    colorMapping,
-    "grey",
-  );
-  return [color, legendRows];
-}
-
-function getColorMappingAndLegend(
-  keys: string[],
-  colorList: string[],
-): [{ [key: string]: string }, [string, string][]] {
-  let legendRows: [string, string][] = [];
-  let colorMapping: { [key: string]: string } = {};
-  let i = 0;
-  for (let x of keys) {
-    let color = colorList[i++ % colorList.length];
-    colorMapping[x] = color;
-    legendRows.push([x, color]);
-  }
-
-  return [colorMapping, legendRows];
-}
--- src/lib/browse/stores.ts
+++ src/lib/browse/stores.ts
@@ -1,28 +1,4 @@
 import { writable, type Writable } from "svelte/store";
-import type { Schemes, SchemeData } from "types";
-
-// TODO Bundle this into one type, so it's easier to plumb around
-export const atfSchemesGj: Writable<Schemes> = writable({
-  type: "FeatureCollection",
-  features: [],
-  schemes: {},
-});
-export const atfSchemes: Writable<Map<string, SchemeData>> = writable(
-  new Map(),
-);
-export const filterAtfInterventionText: Writable<string> = writable("");
-export const filterAtfSchemeText: Writable<string> = writable("");
-
-export const lcwipSchemesGj: Writable<Schemes> = writable({
-  type: "FeatureCollection",
-  features: [],
-  schemes: {},
-});
-export const lcwipSchemes: Writable<Map<string, SchemeData>> = writable(
-  new Map(),
-);
-export const filterLcwipInterventionText: Writable<string> = writable("");
-export const filterLcwipSchemeText: Writable<string> = writable("");
 
 export const interactiveMapLayersEnabled: Writable<boolean> = writable(true);
 export let controls: Writable<HTMLDivElement | null> = writable(null);
