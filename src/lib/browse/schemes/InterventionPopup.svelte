74557f3b379b687ba2e7342fa1e7cf49c8a396f9
Dustin Carlino <dabreegster@gmail.com>
2024-10-02 17:29:41 +0100

    Fix map popups -- start scrolled to the top

--- src/lib/browse/schemes/InterventionPopup.svelte
+++ src/lib/browse/schemes/InterventionPopup.svelte
@@ -8,6 +8,7 @@
   import DescribePipelineTiming from "./DescribePipelineTiming.svelte";
   import DescribePipelineBudget from "./DescribePipelineBudget.svelte";
   import type { Schemes, SchemeData } from "types";
+  import { afterUpdate } from "svelte";
 
   export let props: { [name: string]: any };
   export let schemes: Map<string, SchemeData>;
@@ -16,6 +17,13 @@
   export let filterInterventionText: string;
 
   $: scheme = schemes.get(props.scheme_reference)!;
+
+  let div: HTMLDivElement | undefined;
+  afterUpdate(() => {
+    if (div) {
+      div.scrollTop = 0;
+    }
+  });
 
   // When the user is filtering name/description by freeform text, highlight the matching pieces.
   function highlightFilter(input: string, filter: string): string {
@@ -63,86 +71,91 @@
   }
 </script>
 
-<div style="max-width: 30vw; max-height: 60vh; overflow: auto;">
-  <h2>
-    {@html highlightFilter(props.name, filterInterventionText)} ({props.intervention_type})
-  </h2>
-  {#if props.length_meters}
+{#key props}
+  <div
+    style="max-width: 30vw; max-height: 60vh; overflow: auto;"
+    bind:this={div}
+  >
+    <h2>
+      {@html highlightFilter(props.name, filterInterventionText)} ({props.intervention_type})
+    </h2>
+    {#if props.length_meters}
+      <p>
+        Length: <b>{prettyPrintMeters(props.length_meters)}</b>
+      </p>
+    {/if}
+    {#if props.description}
+      <p>{@html highlightFilter(props.description, filterInterventionText)}</p>
+    {/if}
+
+    {#if props.pipeline}
+      {@const p = props.pipeline}
+      {#if p.atf4_type}
+        <p>
+          ATF4 type: <b>{p.atf4_type}</b>
+        </p>
+      {/if}
+      {#if p.accuracy}
+        <p>
+          Accuracy: <b>{p.accuracy}</b>
+        </p>
+      {/if}
+      {#if p.is_alternative}
+        <p>Represents an alternative route</p>
+      {/if}
+      <DescribePipelineBudget props={p} />
+      <DescribePipelineTiming props={p} />
+    {/if}
+
+    <hr />
+
     <p>
-      Length: <b>{prettyPrintMeters(props.length_meters)}</b>
+      Part of scheme: {@html highlightFilter(
+        scheme.scheme_name ?? "",
+        filterSchemeText,
+      )} ({@html highlightFilter(props.scheme_reference, filterSchemeText)})
     </p>
-  {/if}
-  {#if props.description}
-    <p>{@html highlightFilter(props.description, filterInterventionText)}</p>
-  {/if}
-
-  {#if props.pipeline}
-    {@const p = props.pipeline}
-    {#if p.atf4_type}
-      <p>
-        ATF4 type: <b>{p.atf4_type}</b>
-      </p>
-    {/if}
-    {#if p.accuracy}
-      <p>
-        Accuracy: <b>{p.accuracy}</b>
-      </p>
-    {/if}
-    {#if p.is_alternative}
-      <p>Represents an alternative route</p>
-    {/if}
-    <DescribePipelineBudget props={p} />
-    <DescribePipelineTiming props={p} />
-  {/if}
-
-  <hr />
-
-  <p>
-    Part of scheme: {@html highlightFilter(
-      scheme.scheme_name ?? "",
-      filterSchemeText,
-    )} ({@html highlightFilter(props.scheme_reference, filterSchemeText)})
-  </p>
-  <p>
-    Authority or region: <b>{scheme.browse?.authority_or_region}</b>
-  </p>
-  {#if scheme.browse?.capital_scheme_id}
     <p>
-      Capital scheme ID: <b>{scheme.browse?.capital_scheme_id}</b>
+      Authority or region: <b>{scheme.browse?.authority_or_region}</b>
     </p>
-  {/if}
-  <p>
-    Funding programme: <b>{scheme.browse?.funding_programme}</b>
-  </p>
-  {#if scheme.browse?.current_milestone}
+    {#if scheme.browse?.capital_scheme_id}
+      <p>
+        Capital scheme ID: <b>{scheme.browse?.capital_scheme_id}</b>
+      </p>
+    {/if}
     <p>
-      Current milestone: <b>{scheme.browse?.current_milestone}</b>
+      Funding programme: <b>{scheme.browse?.funding_programme}</b>
     </p>
-  {/if}
-
-  {#if scheme.pipeline}
-    {@const p = scheme.pipeline}
-    {#if p.scheme_type}
+    {#if scheme.browse?.current_milestone}
       <p>
-        Scheme type: <b>{p.scheme_type}</b>
+        Current milestone: <b>{scheme.browse?.current_milestone}</b>
       </p>
     {/if}
-    {#if p.atf4_lead_type}
-      <p>
-        ATF4 lead type: <b>{p.atf4_lead_type}</b>
-      </p>
-    {/if}
-    {#if p.scheme_description}
-      <p>Descripton: {p.scheme_description}</p>
-    {/if}
-    <DescribePipelineBudget props={p} />
-    <DescribePipelineTiming props={p} />
-  {/if}
 
-  <SecondaryButton on:click={showScheme}>
-    Zoom to show entire scheme
-  </SecondaryButton>
-  <SecondaryButton on:click={editScheme}>
-    Edit a copy of this scheme
-  </SecondaryButton>
-</div>
+    {#if scheme.pipeline}
+      {@const p = scheme.pipeline}
+      {#if p.scheme_type}
+        <p>
+          Scheme type: <b>{p.scheme_type}</b>
+        </p>
+      {/if}
+      {#if p.atf4_lead_type}
+        <p>
+          ATF4 lead type: <b>{p.atf4_lead_type}</b>
+        </p>
+      {/if}
+      {#if p.scheme_description}
+        <p>Descripton: {p.scheme_description}</p>
+      {/if}
+      <DescribePipelineBudget props={p} />
+      <DescribePipelineTiming props={p} />
+    {/if}
+
+    <SecondaryButton on:click={showScheme}>
+      Zoom to show entire scheme
+    </SecondaryButton>
+    <SecondaryButton on:click={editScheme}>
+      Edit a copy of this scheme
+    </SecondaryButton>
+  </div>
+{/key}
