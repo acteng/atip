37d57f2943f754c36fca9c14634e2c6d2c461ea8
Dustin Carlino <dabreegster@gmail.com>
2024-10-08 14:30:01 +0100

    Document how the layer management works

--- src/lib/browse/layers/LayerControl.svelte
+++ src/lib/browse/layers/LayerControl.svelte
@@ -17,13 +17,17 @@
     $activeLayers = $activeLayers;
   }
 
-  // TODO explain the remote DOM pattern
+  // The controls for each layer are defined in a layer component's optional
+  // "controls" slot, as well as the generic controls defined below in
+  // this component. The whole DOM element is bound here, then it's
+  // stored centrally in layerLegends . That lets ActiveLayersLegend
+  // later display the proper layer controls in one place.
   let contents: HTMLDivElement | null = null;
 
   $: if (contents) {
     layerLegends.update((l) => {
-      // Initially hide the controls. Before they appear in the legend, the DOM node is mounted on the left sidebar
-      // TODO Can we remove it from the DOM entirely and just store in this map instead?
+      // Initially hide the controls. Since layer components are defined under
+      // the left sidebar, they'd otherwise appear there.
       contents!.style.display = "none";
       l.set(name, contents!);
       return l;
--- src/lib/browse/layers/stores.ts
+++ src/lib/browse/layers/stores.ts
@@ -1,6 +1,8 @@
 import { writable, type Writable } from "svelte/store";
 
+// Per layer, the legend (and controls) shown on the right
 export let layerLegends: Writable<Map<string, HTMLDivElement>> = writable(
   new Map(),
 );
+// The set of layers actively selected
 export let activeLayers: Writable<Set<string>> = writable(new Set());
