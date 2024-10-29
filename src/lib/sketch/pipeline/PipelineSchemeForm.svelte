4a5e833aaa98f3850fb2d14b76dd6a6e01aad39d
Dustin Carlino <dabreegster@gmail.com>
2024-10-29 20:02:05 +0000

    Redesign scheme management

--- src/lib/sketch/pipeline/PipelineSchemeForm.svelte
+++ src/lib/sketch/pipeline/PipelineSchemeForm.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
   import TimingForm from "./TimingForm.svelte";
   import BudgetForm from "./BudgetForm.svelte";
-  import { Modal } from "lib/common";
-  import {
-    DefaultButton,
-    ErrorMessage,
-    Radio,
-    SecondaryButton,
-    TextArea,
-    TextInput,
-  } from "govuk-svelte";
+  import { ErrorMessage, Radio, TextArea, TextInput } from "govuk-svelte";
   import PipelineType from "./PipelineType.svelte";
   import type { Schemes } from "types";
   import type { Writable } from "svelte/store";
+  import { onDestroy } from "svelte";
 
   export let gjSchemes: Writable<Schemes>;
   export let scheme_reference: string;
 
-  let showModal = false;
   // This is just for conveience below, but it means most changes are not
-  // synced to $gjSchemes until finish() is called
+  // synced to $gjSchemes until onDestroy() is called
   let pipeline = $gjSchemes.schemes[scheme_reference].pipeline!;
 
   // Svelte doesn't see nested updates in BudgetForm and TimingForm, so use a
@@ -27,84 +19,62 @@
   let updateError = 0;
   $: errorMessage = checkRequiredValues(pipeline, updateError);
 
+  // No changes in the form are saved until this happens
+  onDestroy(() => {
+    $gjSchemes.schemes[scheme_reference].pipeline = pipeline!;
+    $gjSchemes = $gjSchemes;
+  });
+
   function checkRequiredValues(_: any, updateError: number): string {
     return pipeline.scheme_type && pipeline.status && pipeline.timescale
       ? ""
       : "Missing some required data";
   }
-
-  function onKeyDown(e: KeyboardEvent) {
-    if (showModal && e.key == "Escape") {
-      e.stopPropagation();
-      showModal = false;
-    }
-  }
-
-  // There are 3 ways to close the modal. Clicking the save button or pressing
-  // escape happen here, but clicking outside the modal happens elsewhere. Make
-  // sure we save in all cases.
-  $: if (!showModal) {
-    finish();
-  }
-
-  // No changes in the form are saved until this happens
-  function finish() {
-    $gjSchemes.schemes[scheme_reference].pipeline = pipeline!;
-    $gjSchemes = $gjSchemes;
-  }
 </script>
 
-<svelte:window on:keydown={onKeyDown} />
-
 <ErrorMessage {errorMessage} />
-<SecondaryButton on:click={() => (showModal = true)}>
-  Scheme details
-</SecondaryButton>
-<Modal title="Scheme details" bind:open={showModal}>
-  <TextInput
-    label="Scheme name"
+
+<TextInput
+  label="Scheme name"
+  required
+  bind:value={$gjSchemes.schemes[scheme_reference].scheme_name}
+/>
+
+<fieldset class="govuk-fieldset">
+  <legend class="govuk-fieldset__legend">Basic information</legend>
+
+  <Radio
+    label="Scheme type"
+    choices={[
+      ["cycling route", "Cycling route"],
+      ["walking route", "Walking route"],
+      ["shared-use route", "Shared-use route"],
+      [
+        "cycling and walking route",
+        "Cycling and walking route, not shared use",
+      ],
+      ["area-based scheme", "Area-based scheme"],
+      ["intersection", "Intersection/junction scheme"],
+    ]}
+    inlineSmall
     required
-    bind:value={$gjSchemes.schemes[scheme_reference].scheme_name}
+    bind:value={pipeline.scheme_type}
   />
 
-  <fieldset class="govuk-fieldset">
-    <legend class="govuk-fieldset__legend">Basic information</legend>
+  <PipelineType
+    label="Type of the main intervention"
+    bind:value={pipeline.atf4_lead_type}
+  />
 
-    <Radio
-      label="Scheme type"
-      choices={[
-        ["cycling route", "Cycling route"],
-        ["walking route", "Walking route"],
-        ["shared-use route", "Shared-use route"],
-        [
-          "cycling and walking route",
-          "Cycling and walking route, not shared use",
-        ],
-        ["area-based scheme", "Area-based scheme"],
-        ["intersection", "Intersection/junction scheme"],
-      ]}
-      inlineSmall
-      required
-      bind:value={pipeline.scheme_type}
-    />
+  <TextArea
+    label="Scheme description (150 words max)"
+    bind:value={pipeline.scheme_description}
+  />
+</fieldset>
 
-    <PipelineType
-      label="Type of the main intervention"
-      bind:value={pipeline.atf4_lead_type}
-    />
+<TimingForm data={pipeline} required onUpdate={() => updateError++} />
 
-    <TextArea
-      label="Scheme description (150 words max)"
-      bind:value={pipeline.scheme_description}
-    />
-  </fieldset>
-
-  <TimingForm data={pipeline} required onUpdate={() => updateError++} />
-
-  <BudgetForm data={pipeline} onUpdate={() => updateError++} />
-
-  <DefaultButton on:click={() => (showModal = false)}>Save</DefaultButton>
-</Modal>
+<BudgetForm data={pipeline} onUpdate={() => updateError++} />
 
 <style>
   fieldset {
