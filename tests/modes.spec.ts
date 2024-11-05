517eace6f4090ce78751e7ba1d01cfbaac31f2c4
Dustin Carlino <dabreegster@gmail.com>
2024-11-05 12:25:23 +0000

    Split test files for better perf, and skip flaky tests that're timing out

--- tests/modes.spec.ts
+++ tests/modes.spec.ts
@@ -104,7 +104,8 @@ test("editing geometry of a area works", async () => {
   await page.getByLabel("Description").click();
 });
 
-test("adding interventions, then deleting one, then adding another", async () => {
+// TODO Flaky only on GH Actions
+test.skip("adding interventions, then deleting one, then adding another", async () => {
   await page.getByRole("button", { name: "New route" }).click();
   await clickMap(page, 522, 468);
   await clickMap(page, 192, 513);
@@ -149,89 +150,6 @@ test("escape key works from every mode", async () => {
     await page.keyboard.down("Escape");
     await expectListMode();
   }
-});
-
-test("edit a freehand area, then cancel", async () => {
-  await page.getByRole("button", { name: "New area (freehand)" }).click();
-  await clickMap(page, 500, 500);
-  await clickMap(page, 400, 500);
-  await clickMap(page, 400, 600);
-  await page.getByRole("button", { name: "Finish" }).click();
-
-  await page.getByRole("link", { name: "Untitled area" }).click();
-  await clickMap(page, 430, 530);
-  // This button indicates the mode is working
-  await expect(page.getByRole("button", { name: "Cancel" })).toBeVisible();
-
-  await page.keyboard.down("Escape");
-  await expectListMode();
-});
-
-test("edit a snapped area, then cancel", async () => {
-  await page.getByRole("button", { name: "New area (snapped)" }).click();
-  await clickMap(page, 500, 500);
-  await clickMap(page, 400, 500);
-  await clickMap(page, 400, 600);
-  await page.getByRole("button", { name: "Finish" }).click();
-
-  await page.getByRole("link", { name: "Untitled area" }).click();
-  await clickMap(page, 430, 515);
-  // This button indicates the mode is working
-  await expect(page.getByRole("button", { name: "Cancel" })).toBeVisible();
-
-  await page.keyboard.down("Escape");
-  await expectListMode();
-});
-
-// TODO Edit each type of object without saving, and verify the edits are
-// retained. Or cancel and make sure they're reverted. (How to test for
-// geometry changes?)
-
-test("edit a route, then cancel", async () => {
-  await page.getByRole("button", { name: "New route" }).click();
-  await clickMap(page, 500, 500);
-  await clickMap(page, 400, 500);
-  await page.getByRole("button", { name: "Finish" }).click();
-
-  await page
-    .getByRole("link", { name: "Route from ??? and Beach Green to ???" })
-    .click();
-  await clickMap(page, 442, 492);
-  // This button indicates the mode is working
-  await expect(page.getByRole("button", { name: "Cancel" })).toBeVisible();
-
-  await page.keyboard.down("Escape");
-  await expectListMode();
-});
-
-test("the viewport changes only once when opening a form", async () => {
-  let defaultViewport = new URL(page.url()).hash;
-
-  // Create a point, and make sure the viewport hasn't changed
-  await page.getByRole("button", { name: "New point" }).click();
-  await clickMap(page, 500, 500);
-  await expect(new URL(page.url()).hash).toEqual(defaultViewport);
-
-  // Open the form from the sidebar. The viewport should change
-  await page.getByRole("link", { name: "Untitled point" }).click();
-  // The zoom effect is set to happen over 500ms, so wait a bit
-  await page.waitForTimeout(700);
-  let pointViewport = new URL(page.url()).hash;
-  await expect(pointViewport).not.toEqual(defaultViewport);
-
-  // Zoom in on the map without closing the form. The viewport should again differ.
-  await page.getByRole("region", { name: "Map" }).hover();
-  await page.mouse.wheel(0, -100);
-  // The above scroll doesn't auto-wait for anything
-  await page.waitForTimeout(1000);
-  let customViewport = new URL(page.url()).hash;
-  await expect.soft(customViewport).not.toEqual(pointViewport);
-
-  // Interact with the form, which is already open. The viewport should stay the same.
-  await page.getByText("Crossing", { exact: true }).click();
-  // If we're incorrectly changing the viewport here, wait for the effect to happen
-  await page.waitForTimeout(700);
-  await expect(new URL(page.url()).hash).toEqual(customViewport);
 });
 
 // Assert the page is in the main list mode.
