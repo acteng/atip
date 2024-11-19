2cd903f8e1785e112b3cacc7f7c09c3cd43eba67
Dustin Carlino <dabreegster@gmail.com>
2024-11-19 11:36:19 +0000

    Simplify the SS sketches shown as a SB layer -- one layer, not split by ATF/LCWIP.

--- src/lib/browse/schemes/stores.ts
+++ src/lib/browse/schemes/stores.ts
@@ -1,37 +1,29 @@
 import { writable, type Writable } from "svelte/store";
 import type { Schemes } from "types";
 
-// There are four variations -- main and local ATF and LCWIP schemes
+// There are three variations -- ATF, LCWIP, and local (any schema)
 
 // TODO Bundle this into one type, so it's easier to plumb around
-export const mainAtfSchemes: Writable<Schemes> = writable({
+export const atfSchemes: Writable<Schemes> = writable({
   type: "FeatureCollection",
   features: [],
   schemes: {},
 });
-export const filterMainAtfInterventionText: Writable<string> = writable("");
-export const filterMainAtfSchemeText: Writable<string> = writable("");
+export const filterAtfInterventionText: Writable<string> = writable("");
+export const filterAtfSchemeText: Writable<string> = writable("");
 
-export const mainLcwipSchemes: Writable<Schemes> = writable({
+export const lcwipSchemes: Writable<Schemes> = writable({
   type: "FeatureCollection",
   features: [],
   schemes: {},
 });
-export const filterMainLcwipInterventionText: Writable<string> = writable("");
-export const filterMainLcwipSchemeText: Writable<string> = writable("");
+export const filterLcwipInterventionText: Writable<string> = writable("");
+export const filterLcwipSchemeText: Writable<string> = writable("");
 
-export const localAtfSchemes: Writable<Schemes> = writable({
+export const localSchemes: Writable<Schemes> = writable({
   type: "FeatureCollection",
   features: [],
   schemes: {},
 });
-export const filterLocalAtfInterventionText: Writable<string> = writable("");
-export const filterLocalAtfSchemeText: Writable<string> = writable("");
-
-export const localLcwipSchemes: Writable<Schemes> = writable({
-  type: "FeatureCollection",
-  features: [],
-  schemes: {},
-});
-export const filterLocalLcwipInterventionText: Writable<string> = writable("");
-export const filterLocalLcwipSchemeText: Writable<string> = writable("");
+export const filterLocalInterventionText: Writable<string> = writable("");
+export const filterLocalSchemeText: Writable<string> = writable("");
