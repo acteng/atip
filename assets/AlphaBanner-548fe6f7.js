47d49fa62dbd3a67759ef111ee958e8195703d1b
Pete-Y-CS <Pete-Y-CS@users.noreply.github.com>
2024-11-27 09:41:05 +0000

    deploy: f0d7269e5ce869e19c3c37dafbe9d5cefe5125a4

--- assets/AlphaBanner-548fe6f7.js
+++
@@ -1,4 +0,0 @@
-import{S as o,i as r,s as i,g as c,k as l,l as p,v as t,o as g}from"./stores-90314b69.js";function u(n){let a;return{c(){a=c("div"),a.innerHTML=`<p class="govuk-phase-banner__content" style="margin-bottom: 0px"><strong class="govuk-tag govuk-phase-banner__content__tag">Alpha</strong> <span class="govuk-phase-banner__text">This is a new service. Contact <a href="mailto:dcarlino@turing.ac.uk">Dustin</a>
-      and
-      <a href="mailto:Peter.York@activetravelengland.gov.uk">Pete</a>
-      with any feedback.</span></p>`,l(a,"class","govuk-phase-banner")},m(e,s){p(e,a,s)},p:t,i:t,o:t,d(e){e&&g(a)}}}class h extends o{constructor(a){super(),r(this,a,null,u,i,{})}}export{h as A};
