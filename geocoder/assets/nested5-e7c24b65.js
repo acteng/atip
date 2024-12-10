ad7fd4ef3847a5abb94223a84bced7ff5c3689d7
dabreegster <dabreegster@users.noreply.github.com>
2024-12-10 13:29:42 +0000

    deploy: 971c5e65db447568facc88eecc180b1b52930ea9

---
+++ geocoder/assets/nested5-e7c24b65.js
@@ -0,0 +1,26 @@
+import{S as se,i as ae,s as oe,B as le,g as i,c as X,j as n,p as re,k as ce,l as ue,m as Z,n as t,v as pe,t as ee,e as te,o as he,f as ie,q as de,r as me}from"./index-2dccad83.js";import{H as be}from"./Header-52a895a0.js";function ge(H){let e,a,W,o,M,l,q,c,_,u,N,p,L,h,B,d,I,m,P,b,Y,g,R,f,j,v,G,y,V,w,D,C,F,A,J,T,O,x,z,k,K,S,Q,$,U,E,r;return a=new be({}),o=new le({}),{c(){e=i("div"),X(a.$$.fragment),W=n(),X(o.$$.fragment),M=n(),l=i("h1"),l.textContent="Plan Your Active Travel Schemes Accessibility Statement",q=n(),c=i("p"),c.textContent=`This accessibility statement applies to the Plan Your Active Travel Schemes
+    tool, which is for sketching and viewing active travel schemes at various
+    stages of the bidding and development process.`,_=n(),u=i("p"),u.textContent=`This website is run by Active Travel England. It requires use of and
+    interactions with maps.`,N=n(),p=i("ul"),p.innerHTML=`<li>We include multiple options for basemap, allowing the user to decide what
+      works best for them</li> <li>We intend to add user-configurable colour choices for map layers such as
+      boundaries, sketched schemes and trip generators</li> <li><a href="https://mcmw.abilitynet.org.uk/">AbilityNet</a>
+       has advice on making your device easier to use if you have a disability</li>`,L=n(),h=i("h2"),h.textContent="How accessible this website is",B=n(),d=i("p"),d.textContent="We know some parts of the website are not fully accessible:",I=n(),m=i("p"),m.textContent=`This website is run by Active Travel England. It requires use of and
+    interactions with maps.`,P=n(),b=i("ul"),b.innerHTML=`<li>You cannot navigate most of the website using just a keyboard</li> <li>You cannot navigate most of the website using speech recognition software</li> <li>You cannot listen to most of the website using a screen reader (including
+      the most recent versions of JAWS, NVDA and VoiceOver)</li> <li>Colour will be used to convey information such as road width and speed
+      limits, until this is configurable it might present an accessibility
+      issue.</li>`,Y=n(),g=i("h2"),g.textContent="Feedback and contact information",R=n(),f=i("p"),f.innerHTML=`If you need information on this app or to provide further feedback on issues
+    please email <a href="mailto:plan-your-active-travel-schemes@activetravelengland.gov.uk">plan-your-active-travel-schemes@activetravelengland.gov.uk</a>
+    . We’ll consider your request and get back to you in 20 days.`,j=n(),v=i("h2"),v.textContent="Enforcement procedure",G=n(),y=i("p"),y.innerHTML=`The Equality and Human Rights Commission (EHRC) is responsible for enforcing
+    the Public Sector Bodies (Websites and Mobile Applications) (No. 2)
+    Accessibility Regulations 2018 (the ‘accessibility regulations’). If you’re
+    not happy with how we respond to your complaint, <a href="https://www.equalityadvisoryservice.com/">contact the Equality Advisory and Support Service (EASS)</a>
+    .`,V=n(),w=i("h2"),w.textContent="Technical information about this website’s accessibility",D=n(),C=i("p"),C.textContent=`Active Travel England is committed to making its website accessible, in
+    accordance with the Public Sector Bodies (Websites and Mobile Applications)
+    (No. 2) Accessibility Regulations 2018.`,F=n(),A=i("h2"),A.textContent="Compliance status",J=n(),T=i("p"),T.textContent=`This website is not compliant with the Web Content Accessibility Guidelines
+    version 2.2 AA standard, due to the non-compliances listed below.`,O=n(),x=i("ul"),x.innerHTML=`<li>The map information is necessarily visual and it cannot be converted into
+      text content</li> <li>Similarly many interactions with the map cannot be performed by keyboard
+      only</li> <li>Certain interactions with sketching require dragging with mouse or touch
+      pad</li>`,z=n(),k=i("h2"),k.textContent="Non-compliance with the accessibility regulations",K=n(),S=i("p"),S.textContent=`Navigation and accessing information: We use a map to provide proposed
+    infrastructure schemes to visually help ATE teams assess schemes. This is
+    not used for navigational purposes.`,Q=n(),$=i("h2"),$.textContent="Preparation of this accessibility statement",U=n(),E=i("p"),E.textContent=`This statement was prepared on 24th September 2024. We did an internal audit
+    against WCAG guidelines.`,re(l,"margin-top","30px"),ce(e,"class","govuk-width-container govuk-prose")},m(s,ne){ue(s,e,ne),Z(a,e,null),t(e,W),Z(o,e,null),t(e,M),t(e,l),t(e,q),t(e,c),t(e,_),t(e,u),t(e,N),t(e,p),t(e,L),t(e,h),t(e,B),t(e,d),t(e,I),t(e,m),t(e,P),t(e,b),t(e,Y),t(e,g),t(e,R),t(e,f),t(e,j),t(e,v),t(e,G),t(e,y),t(e,V),t(e,w),t(e,D),t(e,C),t(e,F),t(e,A),t(e,J),t(e,T),t(e,O),t(e,x),t(e,z),t(e,k),t(e,K),t(e,S),t(e,Q),t(e,$),t(e,U),t(e,E),r=!0},p:pe,i(s){r||(ee(a.$$.fragment,s),ee(o.$$.fragment,s),r=!0)},o(s){te(a.$$.fragment,s),te(o.$$.fragment,s),r=!1},d(s){s&&he(e),ie(a),ie(o)}}}function fe(H){return de(async()=>{me()}),[]}class ve extends se{constructor(e){super(),ae(this,e,fe,ge,oe,{})}}document.body.className+=" js-enabled"+("noModule"in HTMLScriptElement.prototype?" govuk-frontend-supported":"");new ve({target:document.getElementById("app")});
