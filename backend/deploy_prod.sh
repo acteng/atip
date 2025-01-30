c2abe3469aa199528ae16d6fbcd1bb0782f8aca7
Dustin Carlino <dabreegster@gmail.com>
2025-01-30 10:54:47 +0000

    Make prod deployment script (cherrypicked 39ae0289e15e97968994e3be9fa2dbd635a6045d from main)

---
+++ backend/deploy_prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+# Check for a file only in the root dir
+if [[ ! -f playwright.config.js ]]; then
+  echo "You must run this script from the root directory of the repo"
+  exit
+fi
+
+read -p "Do you want to deploy to prod? (y/n)?" CONFIRM
+if [ "$CONFIRM" != "y" ]; then
+  exit
+fi
+
+# Build the app
+npm ci
+npm run setup-govuk
+VITE_RESOURCE_BASE="https://plan.activetravelengland.gov.uk/data" npm run build
+# On developer machines, there may be very large local copies of layers. Do not deploy these.
+rm -rfv dist/layers dist/private_layers
+
+# Deploy to app engine
+cd backend
+rm -rf dist
+cp -R ../dist .
+gcloud app --project=dft-rlg-schemes-gis-prod deploy app_public_prod.yaml --quiet
