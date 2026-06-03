f22e69105ac9a7922651c5b915babc9068816f3d
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2026-06-03 16:16:05 +0100

    Add docker repository for browse page

---
+++ cloud/browse/docker-repository/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "GCP project"
+  type        = string
+  default     = "dft-rlg-atip-dev"
+}
+
+variable "location" {
+  description = "GCP location"
+  type        = string
+  default     = "europe-west1"
+}
