9c1fb0469fa7ed364d347b1cd12cbaf7ada790fd
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2026-06-11 16:32:42 +0100

    Serve router-snapper file locally for tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  webServer: {
+  webServer: [
+    {
     command: "npx vite --port 8080",
-    port: 8080,
+    url: "http://localhost:8080",
+    env: {
+      "VITE_RESOURCE_BASE": "http://localhost:8001"
+    },
     reuseExistingServer: !process.env.CI,
+  },
+    {
+      command: "npm run test-server",
+      url: "http://localhost:8001",
+    },
+  ],
+  use: {
+    baseURL: 'http://localhost:8080',
   },
   reporter: [["list"], ["playwright-ctrf-json-reporter", {}]],
 });
