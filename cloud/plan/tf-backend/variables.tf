9fd89dc1983e4bf828f469313cf54031dfb81bae
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2026-07-24 14:06:20 +0100

    Add terraform backend GCS storage for PYATS
    Using the dev project whilst we lack a common project.

---
+++ cloud/plan/tf-backend/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "GCP project"
+  type        = string
+  default     = "dft-rlg-schemes-gis-dev"
+}
+
+variable "location" {
+  description = "GCP location"
+  type        = string
+  default     = "europe-west1"
+}
